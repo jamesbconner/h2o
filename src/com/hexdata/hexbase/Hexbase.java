4577d43dc2b009a510d83f0dba2e8ec36e56cd5d
SriSatish <sri@0xdata.com>
2012-07-16 11:03:46 -0700

    init main; init ant build

---
+++ src/com/hexdata/hexbase/Hexbase.java
@@ -0,0 +1,8 @@
+// dummy class for build script
+package com.hexdata.hexbase;
+
+public class Hexbase {
+  public static void main(String[] args){
+	System.out.println("DummyMain");
+  }
+}
---
+++ src/init/init.java
@@ -0,0 +1,47 @@
+package init;
+
+import java.io.File;
+
+/** Initializer class for H2O.
+ * 
+ * Unpacks all the dependencies and H2O implementation from the jar file, sets
+ * the loader to be able to load all the classes properly and then executes the
+ * main method of the H2O class.
+ * 
+ * Does nothing if the H2O is not run from a jar archive. (This *is* a feature,
+ * at least for the time being so that we can continue using different IDEs).
+ *
+ * @author peta
+ */
+public class init {
+  public static void main(String[] args) {
+    Loader loader = Loader.instance();
+    if (loader.runningFromJar()) {
+      System.out.println("Extracting impl ementation jar...");
+      loader.addInternalJar("hexbase_impl.jar");
+
+      System.out.println("Extracting sigar binaries...");
+      loader._binlib = loader.extractInternalFolder("binlib");
+      System.setProperty("org.hyperic.sigar.path",loader.binlib().getAbsolutePath());
+      System.out.println("    "+System.getProperty("org.hyperic.sigar.path"));
+      System.out.println("Extracting dependencies...");
+      loader.addInternalJarFolder("lib",true);
+      //System.load(new File(binlib,"libleveldb.so").getAbsolutePath());
+      //System.load(new File("jar/binlib/libleveldb.so").getAbsolutePath());
+    } else {
+      loader._binlib = new File("lib/binlib");
+      System.out.println("Not running from jar file, skipping jar extractions...");
+      System.setProperty("org.hyperic.sigar.path","jar/binlib");
+      System.out.println(new File("jar/binlib").getAbsolutePath());
+      System.load(new File("jar/binlib/libleveldb.so").getAbsolutePath());
+    }
+    try {
+      Class<?> c = Class.forName("com.hexdata.hexbase.Hexbase",true,loader);
+      c.getMethod("main",String[].class).invoke(null, (Object)args);
+    } catch (Exception e) {
+      System.err.println("Unable to run Hexbase:");
+      throw new Error(e);
+    }
+  }
+  
+}
