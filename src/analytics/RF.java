a0aba76c7ebb644a6daa7a72329e23a8a51022a0
jan vitek <janvite@yahoo.com>
2012-07-27 08:34:05 -0700

    adding RF

---
+++ src/analytics/RF.java
@@ -0,0 +1,8 @@
+package analytics;
+
+public class RF {
+    private final DataAdapter data_;
+    RF(DataAdapter data) { data_ = data;  }
+    
+    DecisionTree[] computeRF() {  return null;   }
+}
