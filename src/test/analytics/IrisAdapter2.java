8d4fc52ef2128533dafaae74c934ba41602d88ef
Tomas Nykodym <tomas.nykodym@gmail.com>
2012-07-28 20:41:40 -0400

    updated iris and poker adapters

--- src/test/analytics/IrisAdapter2.java
+++ src/test/analytics/IrisAdapter2.java
@@ -19,6 +19,15 @@ import analytics.DataAdapter;
 import analytics.RF;
 import analytics.RFBuilder;
 
+/**
+ * Simple adapter for iris dataset.
+ * 
+ * Can be created either with value (arraylet root) from K/V store
+ * or with File (for testing purposes)
+ * 
+ * @author tomas
+ *
+ */
 public class IrisAdapter2 implements DataAdapter {
 
   String[] names = new String[] { "ID", "Sepal.Length", "Sepal.Width",
@@ -39,10 +48,14 @@ public class IrisAdapter2 implements DataAdapter {
 
   String int2Class(int i) {
     switch (i) {
-    case 0:  return "setosa";
-    case 1:  return "versicolor";
-    case 2:  return "virginica";
-    default: throw new Error("unknown class ordinal " + i);
+    case 0:
+      return "setosa";
+    case 1:
+      return "versicolor";
+    case 2:
+      return "virginica";
+    default:
+      throw new Error("unknown class ordinal " + i);
     }
   }
 
@@ -57,7 +70,7 @@ public class IrisAdapter2 implements DataAdapter {
     double sl, sw, pl, pw;
     int class_;
 
-    F(int id,CSVRecord r) {
+    F(int id, CSVRecord r) {
       this.id = id;
       sl = r.sl;
       sw = r.sw;
@@ -70,51 +83,81 @@ public class IrisAdapter2 implements DataAdapter {
   F[] data = null;
   int cur = -1;
 
-  public int numColumns() { return 5; }
+  public int numColumns() {
+    return 5;
+  }
 
-  public boolean isInt(int index) { return index == 0; }
+  public boolean isInt(int index) {
+    return index == 0;
+  }
 
   public int toInt(int index) {
-    if (index == 0)  return data[cur].id;
-    else  throw new Error();
+    if (index == 0)
+      return data[cur].id;
+    else
+      throw new Error();
   }
 
   public double toDouble(int index) {
     if (index > 0 && index <= 4) {
-      if (index == 1) return data[cur].pl;
-      if (index == 2) return data[cur].pw;
-      if (index == 3) return data[cur].sl;
-      if (index == 4) return data[cur].sw;
-    } else if (index == 0) return (double) data[cur].id;
+      if (index == 1)
+        return data[cur].sl;
+      if (index == 2)
+        return data[cur].sw;
+      if (index == 3)
+        return data[cur].pl;
+      if (index == 4)
+        return data[cur].pw;
+    } else if (index == 0)
+      return (double) data[cur].id;
     throw new Error("Accessing column " + index);
   }
 
-  public Object originals(int index) {  return null;  }
-  public void getRow(int index) {  cur = index; }
-  public int numRows() { return data.length; }
-  public int numClasses() {  return 3; }
-  public int dataClass() { return data[cur].class_;  }
+  public Object originals(int index) {
+    return null;
+  }
 
-  public IrisAdapter2(Key k) throws NoSuchFieldException, SecurityException,
+  public void getRow(int index) {
+    cur = index;
+  }
+
+  public int numRows() {
+    return data.length;
+  }
+
+  public int numClasses() {
+    return 3;
+  }
+
+  public int dataClass() {
+    return data[cur].class_;
+  }
+
+  public IrisAdapter2(Value v) throws NoSuchFieldException, SecurityException,
       IllegalArgumentException, IllegalAccessException, CSVParseException,
       IOException {
-    Value v = DKV.get(k);
+    this(v, 0, v.chunks() + 1);
+  }
+
+  public IrisAdapter2(Value v, int fromChunk, int toChunk)
+      throws NoSuchFieldException, SecurityException, IllegalArgumentException,
+      IllegalAccessException, CSVParseException, IOException {
     CSVRecord r = new CSVRecord();
     ArrayList<F> parsedRecords = new ArrayList<F>();
+    CSVParserSetup setup = new CSVParserSetup();
+    setup._parseColumnNames = false;
     int id = 0;
     if (v != null) {
-      for (int i = 0; i < v.chunks(); ++i) {
-        System.out.println("Processing chunk # " + i);
-        ValueCSVRecords<CSVRecord> p1 = new ValueCSVRecords<CSVRecord>(v, i, r,
-            new String[] { "sl", "sw", "pl", "pw", "class_" });
-        for (CSVRecord x : p1) {
-          parsedRecords.add(new F(id++,x));
-        }
+      ValueCSVRecords<CSVRecord> p1 = new ValueCSVRecords<CSVRecord>(v,
+          fromChunk, toChunk, r, new String[] { "sl", "sw", "pl", "pw",
+              "class_" }, setup);
+      for (CSVRecord x : p1) {
+        parsedRecords.add(new F(id++, x));
       }
       data = new F[parsedRecords.size()];
       data = parsedRecords.toArray(data);
     } else
-      throw new Error("no such key in K/V store");
+      throw new IllegalArgumentException("passed value can not be null");
   }
 
   public IrisAdapter2(File f) throws NoSuchFieldException, SecurityException,
@@ -129,35 +172,55 @@ public class IrisAdapter2 implements DataAdapter {
             "class_" }, setup);
     int id = 0;
     for (CSVRecord x : p1) {
-      parsedRecords.add(new F(id++,x));
+      parsedRecords.add(new F(id++, x));
     }
     data = new F[parsedRecords.size()];
     data = parsedRecords.toArray(data);
   }
 
-  public static void main(String[] _) throws Exception {
-    IrisAdapter2 data = new IrisAdapter2(new File("C:\\datasets\\iris.data"));    
-    System.out.println("there are " + data.numRows() + " rows and " + data.numColumns() + " columns");
-    for(int i = 0; i < data.numRows(); ++i){
-      data.getRow(i);
-      for(int j = 0; j < data.numColumns(); ++j){        
-        System.out.print(data.isInt(j)?data.toInt(j):data.toDouble(j));
-        System.out.print(", ");
+  /**
+   *  for debuging purposes...
+   * @param args list of filenames to be processed
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception {
+    for (String path : args) {
+      System.out.print("parsing " + path + "...");
+      File f = new File(path);
+      if (!f.exists()) {
+        System.out.println("file not found");
+        continue;
       }
-      System.out.println(data.dataClass());
+      IrisAdapter2 data = new IrisAdapter2(f);
+      System.out.println("done");
+      System.out.println("there are " + data.numRows() + " rows and "
+          + data.numColumns() + " columns");
+      for (int i = 0; i < data.numRows(); ++i) {
+        data.getRow(i);
+        for (int j = 0; j < data.numColumns(); ++j) {
+          System.out.print(data.isInt(j) ? data.toInt(j) : data.toDouble(j));
+          System.out.print(", ");
+        }
+        System.out.println(data.dataClass());
+      }
+      RF rf = new RF(data);
+      rf.compute(100, new IrisBuilder2(67436482,data));
     }
-    
-    RF rf = new RF(data);
-    rf.compute(100, new IrisBuilder2(67436482,data));
   }
 }
 
 class IrisBuilder2 extends RFBuilder {
-  protected IrisBuilder2(long seed, DataAdapter data) { super(seed, data); }
+  protected IrisBuilder2(long seed, DataAdapter data) {
+    super(seed, data);
+  }
+
   @Override
   protected void createStatistic(ProtoNode node, int[] columns) {
     node.addStatistic(new AverageStatistic(columns, 3));
   }
+
   @Override
-  protected int numberOfFeatures(ProtoNode node, ProtoTree tree) { return 3; }
+  protected int numberOfFeatures(ProtoNode node, ProtoTree tree) {
+    return 3;
+  }
 }
--- src/test/analytics/PokerAdapter.java
+++ src/test/analytics/PokerAdapter.java
@@ -7,50 +7,131 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import water.DKV;
+import water.Key;
+import water.Value;
 import water.csv.CSVParser.CSVParseException;
 import water.csv.CSVParser.CSVParserSetup;
 import water.csv.ValueCSVRecords;
 import analytics.DataAdapter;
 
-public class PokerAdapter implements DataAdapter{
+/**
+ * Simple adaptor for Poker dataset.
+ * 
+ * Can be invoked with Value (arraylet root) and arraylet indexes or with File (for convenience when testing)
+ * If creating with arraylet range, note that the last index should point to two places after the last processed chunk
+ *  
+ * 
+ * @author tomas
+ *
+ */
+public class PokerAdapter implements DataAdapter {
 
-  int [][] data;  
+  int[][] data;
   int currentRow;
   HashSet<Integer> _classes;
+
+  public void getRow(int index) {
+    currentRow = index;
+  }
+
+  public int numRows() {
+    return data.length;
+  }
+
+  public int numColumns() {
+    return data[0].length - 1;
+  }
+
+  public boolean isInt(int index) {
+    return true;
+  }
+
+  public int toInt(int index) {
+    return data[currentRow][index];
+  }
+
+  public double toDouble(int index) {
+    return data[currentRow][index];
+  }
+
+  public Object originals(int index) {
+    return null;
+  }
+
+  public int numClasses() {
+    return _classes.size();
+  }
+
+  public int dataClass() {
+    return data[currentRow][data[currentRow].length - 1];
+  }
+
+  public PokerAdapter(Value v) throws NoSuchFieldException, SecurityException,
+  IllegalArgumentException, IllegalAccessException, CSVParseException,
+  IOException {
+    this(v,0,v.chunks()+1);
+  }
   
-  public void getRow(int index) {  currentRow = index;  }
-  public int numRows() { return data.length;  }
-  public int numColumns() { return data[0].length-1; }
-  public boolean isInt(int index) {  return true;  }
-  public int toInt(int index) { return data[currentRow][index]; }
-  public double toDouble(int index) { return data[currentRow][index]; }
-  public Object originals(int index) { return null; }
-  public int numClasses() { return _classes.size(); }
-  public int dataClass() {return data[currentRow][data[currentRow].length-1]; }
-  
-  public PokerAdapter(File inputFile) throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException, FileNotFoundException, CSVParseException, IOException{
-    int[] r = new int[11];  
+  public PokerAdapter(Value v, int chunkFrom, int chunkTo) throws NoSuchFieldException, SecurityException,
+      IllegalArgumentException, IllegalAccessException, CSVParseException,
+      IOException {
+    int[] r = new int[11];
     ArrayList<int[]> parsedRecords = new ArrayList<int[]>();
     CSVParserSetup setup = new CSVParserSetup();
     setup._parseColumnNames = false;
-    ValueCSVRecords<int[]> p1 = new ValueCSVRecords<int[]>(
-        new FileInputStream(inputFile), r, new String[] { "id", "sl", "sw", "pl", "pw", "class_" }, setup);
-    for (int[] x : p1) parsedRecords.add(x.clone());
+    
+    if (v != null) {
+      ValueCSVRecords<int[]> p1 = new ValueCSVRecords<int[]>(v,chunkFrom, chunkTo, r, null, setup);
+      for (int[] x : p1)
+        parsedRecords.add(x.clone());
+      data = new int[parsedRecords.size()][];
+      data = parsedRecords.toArray(data);
+    } else
+      throw new Error("no such key in K/V store");
+  }
+
+  public PokerAdapter(File inputFile) throws NoSuchFieldException,
+      SecurityException, IllegalArgumentException, IllegalAccessException,
+      FileNotFoundException, CSVParseException, IOException {
+    int[] r = new int[11];
+    ArrayList<int[]> parsedRecords = new ArrayList<int[]>();
+    CSVParserSetup setup = new CSVParserSetup();
+    setup._parseColumnNames = false;
+    ValueCSVRecords<int[]> p1 = new ValueCSVRecords<int[]>(new FileInputStream(
+        inputFile), r, null, setup);
+    for (int[] x : p1)
+      parsedRecords.add(x.clone());
     data = new int[parsedRecords.size()][];
     data = parsedRecords.toArray(data);
   }
-    
-  public static void main(String[] _) throws Exception {
-    PokerAdapter data = new PokerAdapter(new File("C:\\datasets\\poker-hand-testing.data"));    
-    System.out.println("there are " + data.numRows() + " rows and " + data.numColumns() + " columns");
-    for(int i = 0; i < data.numRows(); ++i){
-      data.getRow(i);
-      for(int j = 0; j < data.numColumns(); ++j){        
-        System.out.print(data.toInt(j));
-        System.out.print(", ");
+
+  /**
+   * for testing...
+   * 
+   * @param args list of filenames to be processed
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception {
+    for (String path : args) {
+      System.out.print("parsing " + path + "...");
+      File f = new File(path);
+      if (!f.exists()) {
+        System.out.println("file not found!");
+        continue;
       }
-      System.out.println(data.dataClass());
+      PokerAdapter data = new PokerAdapter(f);
+      System.out.println("done");
+      System.out.println("there are " + data.numRows() + " rows and "
+          + data.numColumns() + " columns");
+      for (int i = 0; i < data.numRows(); ++i) {
+        data.getRow(i);
+        for (int j = 0; j < data.numColumns(); ++j) {
+          System.out.print(data.toInt(j));
+          System.out.print(", ");
+        }
+        System.out.println(data.dataClass());
+      }
     }
   }
-  
 }
