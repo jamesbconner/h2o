f3632c30bef6b28cf1cccd869a4ce65fd0ce3ede
jan vitek <janvite@yahoo.com>
2012-07-31 10:52:17 -0700

    minor

--- src/test/analytics/IrisAdapterLarge.java
+++ src/test/analytics/IrisAdapterLarge.java
@@ -170,7 +170,7 @@ public class IrisAdapterLarge extends DataAdapter {
       new F(149, 6.2, 3.4, 5.4, 2.3, "virginica"),
       new F(150, 5.9, 3.0, 5.1, 1.8, "virginica") };
 
-  int ROWMULT = 100000;
+  int ROWMULT = 1000000;
   F[] data = new F[data0.length*ROWMULT];
   
   IrisAdapterLarge() {
