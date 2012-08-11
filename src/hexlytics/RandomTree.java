e83a5f27a8ee8ad56df89d75239bdde9cef070b0
thepeta <thepeta@0xdata.com>
2012-08-11 03:42:38 -0700

    Trees can now serialize and deserialize themselves to byte arrays.

--- src/hexlytics/RandomTree.java
+++ src/hexlytics/RandomTree.java
@@ -16,9 +16,22 @@ public class RandomTree {
   private static String statistic_ = "Numeric"; // Default choice
   int serializedSize_ = 0;
   
+  public RandomTree() {
+  }
+  
+  /** Creates the tree and immediately computes it. */
+  public RandomTree(Data data) {
+    compute(data);
+  }
+  
+  /** Creates the tree from serialized data. */
+  public RandomTree(byte[] from, int offset) {
+    tree_ = deserializeNode(from, offset+4).result;
+  }
+  
   //public RandomTree(Data data) { data_=data;  }    
 
-  public void compute(Data data) { 
+  public final void compute(Data data) { 
     long t = System.currentTimeMillis(); 
     //tree_ = new Root();
     //compute(data,tree_,0);    
@@ -27,7 +40,7 @@ public class RandomTree {
   }
   
   /** for given data creates a node and returns it. */
-  protected INode compute_(Data d) {
+  protected final INode compute_(Data d) {
     int classOf = -1;
     for(Row r : d) 
        if (classOf==-1)
@@ -38,13 +51,13 @@ public class RandomTree {
            break;
          }
     if (classOf!=-1) { 
-      serializedSize_ += 4;
+      serializedSize_ += LeafNode.SERIALIZED_SIZE;;  
       return new LeafNode(classOf);
     } else {
       Statistic s = Statistic.make(statistic_, d);  
       Split best = s.best();
       if (best == null) {
-        serializedSize_ += 4;
+        serializedSize_ += LeafNode.SERIALIZED_SIZE; 
         return new LeafNode(s.classOf());
         //n.set(direction, new LeafNode(s.classOf()));            
       } else {
@@ -53,7 +66,7 @@ public class RandomTree {
         d.filter(best,res);
         nd.set(0,compute_(res[0]));
         nd.set(1,compute_(res[1]));
-        serializedSize_ += 12;
+        serializedSize_ += Node.SERIALIZED_SIZE; 
         return nd;
       }
     }
@@ -96,17 +109,25 @@ public class RandomTree {
  }
  
  /** Leaf node that for any row returns its the data class it belongs to. */
- static class LeafNode extends INode {    
-    int class_ = -1;    // A category reported by the inner node
-    LeafNode(int c)                 { class_ = c; }
-    public int classify(double[] v) { return class_; }
-    public String toString()        { return "["+class_+"]"; }
+ static class LeafNode extends INode {   
+   /** Type identifier of the node in the serialization */
+   public static final byte NODE_TYPE = 0;
+   /** Size of the serialized node. byte type and int class. */
+   public static final int SERIALIZED_SIZE = 5;
+   int class_ = -1;    // A category reported by the inner node
+   LeafNode(int c)                 { class_ = c; }
+   public int classify(double[] v) { return class_; }
+   public String toString()        { return "["+class_+"]"; }
  }
 
  
  /** Inner node of the decision tree. Contains a list of subnodes and the
   * classifier to be used to decide which subtree to explore further. */
  static class Node extends INode {
+   /** Type identifier of the node in the serialization */
+   public static final byte NODE_TYPE = 1;
+   /** Size of the serialized node. Byte tupe, int column and double value. */
+   public static final int SERIALIZED_SIZE = 13;
    final int column_;
    final double value_;
    INode l_, r_;
@@ -145,7 +166,63 @@ public class RandomTree {
   
   /** Returns the size required for the tree to serialize. In bytes. */
   public int serializedSize() {
-    return serializedSize_;
+    return 4 + serializedSize_; // 4 goes for the length
+  }
+  
+  /** Serializes the tree to the given byte array at specified offset. */
+  public void serialize(byte[] to, int offset) {
+    offset += Utils.set4(to,offset,serializedSize_);
+    if (serializedSize_!=0)
+      serializeNode(tree_,to,offset);
+  }
+  
+  /** Serializes the given node recursively to the given byte array. */
+  private int serializeNode(INode node, byte[] to, int offset) {
+    //System.out.println(offset);
+    if (node instanceof LeafNode) {
+      to[offset++] = LeafNode.NODE_TYPE;
+      offset += Utils.set4(to,offset,((LeafNode)node).class_);
+    } else {
+      to[offset++] = Node.NODE_TYPE;
+      offset += Utils.set4(to,offset,((Node)node).column_);
+      offset += Utils.set8d(to,offset,((Node)node).value_);
+      offset = serializeNode(((Node)node).l_,to,offset);
+      offset = serializeNode(((Node)node).r_,to,offset);
+    }
+    return offset;
+  }
+  
+  private class DeserializationResult {
+    public final INode result;
+    public final int offset;
+    DeserializationResult(INode result, int offset) {
+      this.result = result;
+      this.offset = offset;
+    }
+  }
+  
+  /** Deserializes the nodes and returns them. */
+  private DeserializationResult deserializeNode(byte[] from, int offset) {
+    if (from[offset]==LeafNode.NODE_TYPE) {
+      offset += 1;
+      serializedSize_ += LeafNode.SERIALIZED_SIZE;
+      return new DeserializationResult(new LeafNode(Utils.get4(from,offset)),offset+4);      
+    } if (from[offset]==Node.NODE_TYPE) {
+      offset += 1;
+      serializedSize_ += Node.SERIALIZED_SIZE;
+      int column = Utils.get4(from,offset);
+      offset += 4;
+      double value = Utils.get8d(from,offset);
+      offset += 8;
+      Node n = new Node(column,value);
+      DeserializationResult r = deserializeNode(from,offset);
+      n.set(0,r.result);
+      r = deserializeNode(from,r.offset);
+      n.set(1,r.result);
+      return new DeserializationResult(n, r.offset);
+    } else {
+      throw new Error("Unrecognized node type "+from[offset]+" in deserialized tree");
+    }
   }
 }
 
--- src/hexlytics/tests/Iris.java
+++ src/hexlytics/tests/Iris.java
@@ -35,12 +35,17 @@ public class Iris {
       RandomTree rf = new RandomTree();
       rf.compute(train);
       rf.classify(valid, score);
+      byte[] b = new byte[rf.serializedSize()];
+      rf.serialize(b,0);
+      RandomTree rtt = new RandomTree(b,0);
+      System.out.println("Orginial: "+rf.tree());
+      System.out.println("Copy:     "+rtt.tree());
       System.out.println(i+" | err= "+Utils.p5d(RandomTree.score(valid, score)) +" "+ rf.tree());
     } 
-    RandomForest rf = new RandomForest(0.6);
-    rf.addTrees(d, 100000,2);
-    System.out.println("We now have "+rf.numTrees()+" trees in the forest");
-    System.out.println("Score on full set: "+rf.score(d));
+    //RandomForest rf = new RandomForest(0.6);
+    //rf.addTrees(d, 100000,2);
+    //System.out.println("We now have "+rf.numTrees()+" trees in the forest");
+    //System.out.println("Score on full set: "+rf.score(d));
   }
   
   class F {  int id; double sl, sw, pl, pw;   int class_; 
