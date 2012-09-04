028c3ad4d5db692af461397e759c03ec9309a4ba
Matt Fowles <matt.fowles@gmail.com>
2012-09-03 18:09:34 -0700

    commented out code, die in a fire

--- src/water/parser/CSVParserKV.java
+++ src/water/parser/CSVParserKV.java
@@ -44,14 +44,6 @@ public class CSVParserKV<T> implements Iterable<T>, Iterator<T> {
       _length = length;
     }
 
-    // private int chunkIdx(){
-    // int idx = (int)(_offset >> ValueArray.LOG_CHK);
-    // if((idx == _nextChunkIdx) && (_data.length > (1 << ValueArray.LOG_CHK))){
-    // return idx - 1; // the last chunk can be larger!
-    // }
-    // return idx;
-    // }
-
     private int[] chunkIdxAndOffset() {
       int[] res = new int[2];
       res[1] = (int) (_offset & ((1 << ValueArray.LOG_CHK) - 1));
