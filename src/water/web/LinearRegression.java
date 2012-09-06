e66f426cc26c8e06f26da5aca28a6343e02244db
srisatish <sri@0xdata.com>
2012-09-06 10:25:13 -0700

    stdout add for redirect-test

--- src/water/web/LinearRegression.java
+++ src/water/web/LinearRegression.java
@@ -7,6 +7,7 @@ public class LinearRegression extends H2OPage {
     return ServletUtil.serveTwoParams(args, new ServletUtil.RunnableTask() {
       @Override
       public String run(ValueArray ary, int colA, int colB) {
+	System.out.println("Running LR");
         return hexlytics.LinearRegression.run(ary,colA,colB);
       }
     });
