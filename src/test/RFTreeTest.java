987b9237f0aa24a97fc04423cb2cec56a18e01f2
srisatish <sri@0xdata.com>
2012-09-06 11:00:30 -0700

    fix test

--- src/test/RFTreeTest.java
+++ src/test/RFTreeTest.java
@@ -3,7 +3,7 @@ package test;
 import hexlytics.rf.RandomForest;
 
 import java.io.File;
-
+import hexlytics.rf.DRF;
 import org.junit.BeforeClass;
 
 import water.*;
@@ -14,10 +14,12 @@ public class RFTreeTest {
   }
   
   public static void main(String[] args) throws Exception {
-    Key fileKey = TestUtil.load_test_file(new File("smalldata/poker/poker-hand-testing.data"));
+    H2O.main(new String[] {});
+    if(args.length==0) args = new String[] { "smalldata/poker/poker-hand-testing.data" };
+    Key fileKey = TestUtil.load_test_file(new File(args[0]));
     ValueArray va = TestUtil.parse_test_key(fileKey);
     DKV.remove(fileKey); // clean up and burn
-//    LocalBuilder builder = RandomForest.web_main(va, 10, 100, .15, true);
-//    builder.
+    DRF.web_main(va, 10, 100, .15, false);
+
   }
 }
