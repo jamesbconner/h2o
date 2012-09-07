70451442a87e8de82296dc40d59bce8f9820b98c
thepeta <thepeta@0xdata.com>
2012-09-07 10:17:52 -0700

    EntropyStatistic has its own file now.

---
+++ src/hexlytics/rf/EntropyStatistic.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hexlytics.rf;
+
+/**
+ *
+ * @author peta
+ */
+public class EntropyStatistic {
+  
+}
