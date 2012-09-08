fab3abf24e4415c58103cd1b83349e65ed496e2f
srisatish <sri@0xdata.com>
2012-09-08 13:09:29 -0700

    pretty up

--- src/test/RandomForestTest.java
+++ src/test/RandomForestTest.java
@@ -19,19 +19,19 @@ public class RandomForestTest {
   }
   
   @org.junit.Test public void testSmallData() {
-		System.out.println("Running RandomForest on small data sets");
-    String[] datafiles = new String[] { "smalldata/poker/poker100","smalldata/iris/iris2.csv"};
-    for (int i=0; i<datafiles.length; i++){
-		 System.out.println("RandomForest for "+datafiles[i]);
-      Key fileKey = TestUtil.load_test_file(new File(datafiles[i]));
-      ValueArray va = TestUtil.parse_test_key(fileKey);
-
-           Key entropyR = DRF.web_main(va, 10, 100, .15, Tree.StatType.oldEntropy);
-           Key    giniR = DRF.web_main(va, 10, 100, .15, Tree.StatType.gini);
-
-			UKV.remove(fileKey); // clean up and burn
-			UKV.remove(entropyR);
-			UKV.remove(giniR);
+  System.out.println("Running RandomForest on small data sets");
+  String[] datafiles = new String[] { "smalldata/poker/poker100","smalldata/iris/iris2.csv"};
+  for (int i=0; i<datafiles.length; i++){
+    System.out.println("RandomForest for "+datafiles[i]);
+    Key fileKey = TestUtil.load_test_file(new File(datafiles[i]));
+    ValueArray va = TestUtil.parse_test_key(fileKey);
+    try { Thread.sleep(100); }        // sleep 100msec, to let parse finish & test again
+    catch( InterruptedException ie ) {}
+    Key entropyR = DRF.web_main(va, 10, 100, .15, Tree.StatType.oldEntropy);
+    Key    giniR = DRF.web_main(va, 10, 100, .15, Tree.StatType.gini);
+    UKV.remove(fileKey); // clean up and burn
+    UKV.remove(entropyR);
+    UKV.remove(giniR);
     }
   }
 }
