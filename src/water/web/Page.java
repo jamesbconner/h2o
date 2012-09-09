7912bc0134613f8462303a4666e4551dd5084dc1
Matt Fowles <matt.fowles@gmail.com>
2012-09-08 21:26:08 -0700

    Remove dead code

--- src/water/web/Page.java
+++ src/water/web/Page.java
@@ -1,20 +1,12 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package water.web;
 
 import java.util.Properties;
 
-/**
- *
- * @author peta
- */
 public abstract class Page {
 
   public String[] requiredArguments() {
     return null;
   }
-  
+
   public abstract Object serve(Server server, Properties args);
 }
