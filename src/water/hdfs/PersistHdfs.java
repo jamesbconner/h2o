e5bcb33bb071aa0154d91ff2034577ad81f02578
Matt Fowles <matt.fowles@gmail.com>
2012-09-11 12:43:56 -0700

    HSFS should not persist when not used

--- src/water/hdfs/PersistHdfs.java
+++ src/water/hdfs/PersistHdfs.java
@@ -18,7 +18,7 @@ public abstract class PersistHdfs {
   private final static Configuration _conf;
   private static FileSystem _fs;
   private static Path _root;
-  
+
   static {
     if( H2O.OPT_ARGS.hdfs_config!=null ) {
       _conf = new Configuration();
@@ -50,11 +50,11 @@ public abstract class PersistHdfs {
       }
     }
   }
-  
+
   public static void refreshHDFSKeys(){
-    loadPersistentKeysFromFolder(_root, "");
+    if( _fs != null ) loadPersistentKeysFromFolder(_root, "");
   }
-  
+
   private static int loadPersistentKeysFromFolder(Path folder, String prefix) {
     int num=0;
     try {
@@ -83,41 +83,41 @@ public abstract class PersistHdfs {
     }
     return num;
   }
-  
+
   // file implementation -------------------------------------------------------
 
-  // Decodes the given file on a prefixed path to a key. 
+  // Decodes the given file on a prefixed path to a key.
   // TODO As of now does not support filenames (including prefix) larger than
   // 512 bytes.
   static private final String KEY_PREFIX="hdfs:/";
   static private final int KEY_PREFIX_LENGTH=KEY_PREFIX.length();
-    
+
   private static String path2KeyStr(Path p){
     if(p.getParent().depth() == _root.depth()) return KEY_PREFIX + Path.SEPARATOR + p.getName();
     return path2KeyStr(p.getParent()) + Path.SEPARATOR + p.getName();
-  } 
-  
+  }
+
   private static Key decodeFile(Path p, String prefix) {
     String kname = KEY_PREFIX+prefix+Path.SEPARATOR+p.getName();
     assert (kname.length() <= 512);
     // all HDFS keys are HDFS-kind keys
     return Key.make(kname.getBytes());
   }
-  
-  // Returns the path for given key. 
+
+  // Returns the path for given key.
   // TODO Something should be done about keys whose value is larger than 512
   // bytes, for now, they are not supported.
   static Path getPathForKey(Key k) {
     final int len = KEY_PREFIX_LENGTH + 1; // Strip key prefix & leading slash
-    String s = new String(k._kb,len, k._kb.length-len);    
+    String s = new String(k._kb,len, k._kb.length-len);
     return new Path(_root, s);
   }
-  
+
   private static Key getKeyforPath(Path p){
     return Key.make(path2KeyStr(p));
   }
-  
-  
+
+
   static public byte[] file_load(Value v, int len) {
     byte[] b =  MemoryManager.allocateMemory(len);
     try {
@@ -143,9 +143,9 @@ public abstract class PersistHdfs {
     } catch( IOException e ) {  // Broken disk / short-file???
       System.out.println("hdfs file_load throws error "+e+" for key "+v._key);
       return null;
-    } 
+    }
   }
-  
+
 //for moving ValueArrays to HDFS
  static void storeChunk(Value v, String path) {
    assert !(v instanceof ValueArray);
@@ -171,9 +171,9 @@ public abstract class PersistHdfs {
      }
    } catch( IOException e ) {
      e.printStackTrace();
-   }    
+   }
  }
- 
+
 
  static void addNewVal2KVStore(String path){
    Path p = new Path(_root,path);
@@ -203,8 +203,8 @@ public abstract class PersistHdfs {
       FSDataOutputStream s = _fs.create(p);
       try {
         byte[] m = v.mem();
-        assert (m == null || m.length == v._max); // Assert not saving partial files 
-        if (m!=null) 
+        assert (m == null || m.length == v._max); // Assert not saving partial files
+        if (m!=null)
           s.write(m);
         v.setdsk();             // Set as write-complete to disk
       } finally {
@@ -213,12 +213,12 @@ public abstract class PersistHdfs {
     } catch( IOException e ) {
     }
   }
-  
+
   static public void file_delete(Value v) {
     // Only the home node does persistence.
     if(v._key._kb[0] == Key.ARRAYLET_CHUNK) return;
     if( !v._key.home() ) return;
-    
+
     // Never store arraylets on HDFS, instead we'll store the entire array.
     assert !(v instanceof ValueArray);
     assert v.mem() == null;     // Upper layers already cleared out
@@ -242,6 +242,6 @@ public abstract class PersistHdfs {
       return val;
     } catch( IOException e ) {  // Broken disk / short-file???
       return null;
-    } 
+    }
   }
 }
