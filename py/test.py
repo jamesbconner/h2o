25e799f2075ee20eec233f2d28490ffb8e067336
Kevin Normoyle <kevin@0xdata.com>
2012-09-12 15:48:12 -0700

    socket to google dns to determine IP address. use len(nodes) everywhere for expected node number. Some info while adding nodes.

--- py/test.py
+++ py/test.py
@@ -2,10 +2,38 @@ import os, json, unittest, time
 import util.h2o as h2o
 import util.asyncproc as proc
 
+# for the ip address detection
+import commands
+import socket
+
 def addNode():
     global nodes
-    h = h2o.H2O('192.168.1.12', 54321 + len(nodes)*3)
+
+    # Hackery: find the ip address that gets you to Google's DNS
+    # Trickiness because you might have multiple IP addresses (Virtualbox)
+    # Should work on both Unix and Windows, for finding IP, and if multiple IPs
+    # will fail if local proxy though.
+    # Maybe update to just connect to our local DNS? DNS uses port 53
+    # Going to google won't work if you're behind a NAT? (home router?)
+    # so what if you're not at 0xdata? Try it, but just use ifconfig result.
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 0))
+    ipaddr = s.getsockname()[0]
+    if (ipaddr.startswith('127')):
+        ipAddress = "127.0.0.1"
+        print "Can't figure it out: Using", ipAddress
+    else:
+        ipAddress = ipaddr
+        print ipAddress, "- Using this IP from socket.gethostname"
+
+        
+    portForH2O = 54321
+    portsPerNode = 3
+    print "Assuming H2O ports start at", portForH2O, "with", portsPerNode, "ports per node"
+    h = h2o.H2O(ipAddress, portForH2O + len(nodes)*portsPerNode)
+    ## print h
     nodes.append(h)
+    print "H2O Node count:", len(nodes)
 
 def runRF(n):
     put = n.put_file('../smalldata/iris/iris2.csv')
@@ -22,12 +50,12 @@ class Basic(unittest.TestCase):
         try:
             proc.clean_sandbox()
             nodes = []
-            addNode()
-            addNode()
-            addNode()
+
+            for i in range(3):
+                addNode()
 
             # give them a few seconds to stabilize
-            nodes[0].stabilize('cloud auto detect', 3,
+            nodes[0].stabilize('cloud auto detect', len(nodes),
                 lambda n: n.get_cloud()['cloud_size'] == len(nodes))
         except:
             for n in nodes: n.terminate()
@@ -45,14 +73,14 @@ class Basic(unittest.TestCase):
 
     def setUp(self):
         pass
+
     def tearDown(self):
         pass
 
     def testBasic(self):
-        self.assertEqual(len(nodes), 3)
         for n in nodes:
             c = n.get_cloud()
-            self.assertEqual(c['cloud_size'], 3, 'inconsistent cloud size')
+            self.assertEqual(c['cloud_size'], len(nodes), 'inconsistent cloud size')
 
     def testRF(self):
         runRF(nodes[0])
