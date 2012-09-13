55507ebfee2962b762d19c6a451ed7d12640e7ea
thepeta <thepeta@0xdata.com>
2012-09-13 13:28:01 -0700

    Entropy exclusionSplit implemented with new framework.

--- src/hexlytics/rf/EntropyStatistic.java
+++ src/hexlytics/rf/EntropyStatistic.java
@@ -61,49 +61,47 @@ class EntropyStatistic extends Statistic {
   } 
   
   @Override protected Split columnExclusion(int colIndex, Data d) {
-    return null;
-//    // get the weight and left & right distributions, initialize the split info
-//    Arrays.fill(left,0);
-//    System.arraycopy(dist_, 0, right, 0, dist_.length);
-//    double maxReduction = -1;
-//    int bestSplit = -1;
-//    for (int i = 0; i < columnDists_[colIndex].length; ++i) {
-//      double eLeft = 0;
-//      int totLeft = 0;
-//      int totRight = 0;
-//      double eRight = 0;
-//      for (int j = 0; j < excluded.length; ++j) {
-//        double v = columnDists_[colIndex][i][j];
-//        others[j] += excluded[j];
-//        excluded[j] = v;
-//        others[j] -= v;
-//      }
-//      for (double e: excluded)
-//        totExcluded += e;
-//      if (totExcluded == 0)
-//        continue;
-//      for (double e: excluded)
-//        if (e != 0)
-//          eExcluded -= (e/totExcluded) * Math.log(e/totExcluded);
-//      for (double e: others)
-//        totOthers += e;
-//      if (totOthers == 0)
-//        continue;
-//      for (double e: others)
-//        if (e != 0)
-//          eOthers -= (e/totOthers) * Math.log(e/totOthers);
-//      double eReduction = 1 - (eExcluded * totExcluded + eOthers * totOthers) / weight_; 
-//      if (eReduction > maxReduction) {
-//        bestSplit = i;
-//        maxReduction = eReduction;
-//      }
-//    }
-//    // no suitable split can be made, return an impossible const split
-//    if (bestSplit == -1)
-//      return Split.impossible(Utils.maxIndex(dist_,d.random()));
-//    else 
-//      return new Split(colIndex,bestSplit,maxReduction);
+    // get the weight and left & right distributions, initialize the split info
+    Arrays.fill(left,0);
+    System.arraycopy(dist_, 0, right, 0, dist_.length);
+    double maxReduction = -1;
+    int bestSplit = -1;
+    for (int i = 0; i < columnDists_[colIndex].length; ++i) {
+      double eLeft = 0;
+      int totLeft = 0;
+      int totRight = 0;
+      double eRight = 0;
+      for (int j = 0; j < left.length; ++j) {
+        int v = columnDists_[colIndex][i][j];
+        right[j] += left[j];
+        left[j] = v;
+        right[j] -= v;
+      }
+      for (int e: left)
+        totLeft += e;
+      if (totLeft == 0)
+        continue;
+      for (int e: left)
+        if (e != 0)
+          eLeft -= (e/(double)totLeft) * Math.log(e/(double)totLeft);
+      for (int e: right)
+        totRight += e;
+      if (totRight == 0)
+        continue;
+      for (double e: right)
+        if (e != 0)
+          eRight -= (e/(double)totRight) * Math.log(e/totRight);
+      double eReduction = 1 - (eLeft * totLeft + eRight * totRight) / (double)weight_; 
+      if (eReduction > maxReduction) {
+        bestSplit = i;
+        maxReduction = eReduction;
+      }
+    }
+    // no suitable split can be made, return an impossible const split
+    if (bestSplit == -1)
+      return Split.impossible(Utils.maxIndex(dist_,d.random()));
+    else 
+      return Split.exclusion(colIndex,bestSplit,maxReduction);
   } 
-  
   
 } 
