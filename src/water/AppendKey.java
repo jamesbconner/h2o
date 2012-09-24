6799fe28fc046f5f21fd1a58a8be06fcbb5c9e2e
Matt Fowles <matt.fowles@gmail.com>
2012-09-24 17:13:16 -0400

    Good bye hand written serialization

--- src/water/AppendKey.java
+++ src/water/AppendKey.java
@@ -1,11 +1,6 @@
 package water;
 
-import java.io.DataOutputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
 import java.util.Arrays;
-import water.serialization.RTSerializer;
-import water.serialization.RemoteTaskSerializer;
 
 /**
  * Atomic Append of a Key
@@ -13,38 +8,13 @@ import water.serialization.RemoteTaskSerializer;
  * @author <a href="mailto:cliffc@0xdata.com"></a>
  * @version 1.0
  */
-//@RTSerializer(AppendKey.Serializer.class)
 public class AppendKey extends Atomic {
   private final byte[] _bits;
-  public AppendKey( byte[] bits ) { _bits = bits; }
+  public AppendKey( byte[] bits ) { _bits = bits; } // required ctor for serialization
+
   public AppendKey( Key key ) {    // Append a key
     _bits = new byte[key.wire_len()];
     key.write(_bits,0);
-  }
-  public static class Serializer extends RemoteTaskSerializer<AppendKey> {
-    @Override public int wire_len(AppendKey a) { return 4+a._bits.length; }
-
-    @Override
-    public void write(AppendKey task, Stream s) {
-      s.setLen4Bytes(task._bits);
-    }
-
-    @Override
-    public AppendKey read(Stream s) {
-      return new AppendKey(s.getLen4Bytes());
-    }
-
-    @Override public int write( AppendKey a, byte[] buf, int off ) { throw new RuntimeException("Should not be called"); }
-    @Override public AppendKey read(         byte[] buf, int off ) { throw new RuntimeException("Should not be called"); }
-
-    @Override public void write( AppendKey a, DataOutputStream dos ) throws IOException {
-      dos.write(a._bits);
-    }
-    @Override public AppendKey read( DataInputStream dis ) throws IOException {
-      byte[] bits = new byte[dis.readInt()];
-      dis.readFully(bits);
-      return new AppendKey(bits);
-    }
   }
 
   // Just append the bits
