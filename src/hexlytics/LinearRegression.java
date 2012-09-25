4342bd71774d3069707733139295fe847becb52b
Matt Fowles <matt.fowles@gmail.com>
2012-09-25 13:43:13 -0400

    Switch LinearRegression to auto serializer

--- src/hexlytics/LinearRegression.java
+++ src/hexlytics/LinearRegression.java
@@ -1,8 +1,5 @@
 package hexlytics;
-import java.io.*;
 import water.*;
-import water.serialization.RTSerializer;
-import water.serialization.RemoteTaskSerializer;
 
 /**
  * @author cliffc@0xdata.com
@@ -11,7 +8,7 @@ public abstract class LinearRegression {
 
   static public String run( ValueArray ary, int colA, int colB ) {
     StringBuilder sb = new StringBuilder();
-    
+
     sb.append("Linear Regression of ").append(ary._key).append(" between ").
       append(colA).append(" and ").append(colB);
 
@@ -60,7 +57,7 @@ public abstract class LinearRegression {
     sb.append("<p>std error of beta_0 = ").append(Math.sqrt(svar0));
     svar0 = svar * lr._sumX2 / (n * lr._XXbar);
     sb.append("<p>std error of beta_0 = ").append(Math.sqrt(svar0));
-    
+
     sb.append("<p>SSTO = ").append(lr._YYbar);
     sb.append("<p>SSE  = ").append(lr._rss);
     sb.append("<p>SSR  = ").append(lr._ssr);
@@ -68,7 +65,6 @@ public abstract class LinearRegression {
     return sb.toString();
   }
 
-  @RTSerializer(LR_Task.Serializer.class)
   public static class LR_Task extends MRTask {
     Key _arykey;                // Main ValueArray key
     int _pass;                  // Pass 1, 2 or 3.
@@ -78,52 +74,6 @@ public abstract class LinearRegression {
     double _Xbar, _Ybar, _XXbar, _YYbar, _XYbar;
     double _beta0, _beta1;
     double _rss, _ssr;
-    public static class Serializer extends RemoteTaskSerializer<LR_Task> {
-      @Override public int wire_len(LR_Task t) { return 3*4+13*8+t._arykey.wire_len(); }
-      @Override public int write( LR_Task t, byte[] buf, int off ) {
-        off += UDP.set4 (buf,off,t._pass);
-        off += UDP.set4 (buf,off,t._colA);
-        off += UDP.set4 (buf,off,t._colB);
-        off += UDP.set8 (buf,off,t._rows);
-        off += UDP.set8d(buf,off,t._sumX);
-        off += UDP.set8d(buf,off,t._sumY);
-        off += UDP.set8d(buf,off,t._sumX2);
-        off += UDP.set8d(buf,off,t._Xbar);
-        off += UDP.set8d(buf,off,t._Ybar);
-        off += UDP.set8d(buf,off,t._XXbar);
-        off += UDP.set8d(buf,off,t._YYbar);
-        off += UDP.set8d(buf,off,t._XYbar);
-        off += UDP.set8d(buf,off,t._beta0);
-        off += UDP.set8d(buf,off,t._beta1);
-        off += UDP.set8d(buf,off,t._rss  );
-        off += UDP.set8d(buf,off,t._ssr  );
-        off += t._arykey.write(buf,off);
-        return off;
-      }
-      @Override public LR_Task read( byte[] buf, int off ) { 
-        LR_Task t = new LR_Task();
-        t._pass = UDP.get4 (buf,(off+=4)-4);
-        t._colA = UDP.get4 (buf,(off+=4)-4);
-        t._colB = UDP.get4 (buf,(off+=4)-4);
-        t._rows = UDP.get8 (buf,(off+=8)-8);
-        t._sumX = UDP.get8d(buf,(off+=8)-8);
-        t._sumY = UDP.get8d(buf,(off+=8)-8);
-        t._sumX2= UDP.get8d(buf,(off+=8)-8);
-        t._Xbar = UDP.get8d(buf,(off+=8)-8);
-        t._Ybar = UDP.get8d(buf,(off+=8)-8);
-        t._XXbar= UDP.get8d(buf,(off+=8)-8);
-        t._YYbar= UDP.get8d(buf,(off+=8)-8);
-        t._XYbar= UDP.get8d(buf,(off+=8)-8);
-        t._beta0= UDP.get8d(buf,(off+=8)-8);
-        t._beta1= UDP.get8d(buf,(off+=8)-8);
-        t._rss  = UDP.get8d(buf,(off+=8)-8);
-        t._ssr  = UDP.get8d(buf,(off+=8)-8);
-        t._arykey  = Key.read(buf,off);
-        return t;
-      }
-      @Override public void write( LR_Task t, DataOutputStream dos ) { throw new Error("do not call"); }
-      @Override public LR_Task read ( DataInputStream  dis ) { throw new Error("do not call"); }
-    }
 
     public void map( Key key ) {
       assert key.home();
