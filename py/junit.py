7382043698e2d5fb15d4604d66cf1c75d069d42e
Kevin Normoyle <kevin@0xdata.com>
2012-09-26 15:20:16 -0700

    updated like util/h2o.py to use -javaagent: plus the h2o.jar, so it will run with jre or jdk (tools.jar issue)

--- py/junit.py
+++ py/junit.py
@@ -7,13 +7,18 @@ class JUnit(unittest.TestCase):
         h2o.clean_sandbox()
 
     def run_junit(self, javaClass, timeout=None):
+        # we don't have the port or ip configuration here 
+        # that util/h2o.py does? Keep this in synch with spawn_h2o there.
+        # also don't have --nosigar here?
         (ps, stdout, stderr) = h2o.spawn_cmd(javaClass, [
-                'java', '-ea',
+                'java', 
                 '-Dh2o.arg.ice_root='+h2o.tmp_dir('ice.'),
-                '-jar', h2o.find_file('build/h2o.jar'),
+                '-javaagent:' + h2o.find_file('build/h2o.jar'),
+                '-ea', '-jar', h2o.find_file('build/h2o.jar'),
                 '-mainClass', 'org.junit.runner.JUnitCore',
                 javaClass
         ])
+
         rc = ps.wait(timeout)
         out = file(stdout).read()
         err = file(stderr).read()
