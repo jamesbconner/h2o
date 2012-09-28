ed41b704796b076f484b74ace09af2ab0d7b9a0c
Kevin Normoyle <kevin@0xdata.com>
2012-09-28 14:11:52 -0700

    changed to use the .gz directly into h2o. no more gunzipping before file is used.

--- py/test2.py
+++ py/test2.py
@@ -17,33 +17,16 @@ class Basic(unittest.TestCase):
         pass
 
     def test_RFhhp(self):
-        # FIX! we're supposed to be support gzip files but seems to fail
-        # normally we .gz for the git, to save space
-        # we do take .zip directly. bug fix needed for .gz
+        # changed to load the .gz directly
+        # don't every gunzip, since we want to test H2O's ability to use *.gz
+        # if someone gunzip's it to look at it and the .gz goes away, they'll have to 
+        # fix that problem in their directory (use git checkout <file>)
+        csvPathnamegz = '../smalldata/hhp_9_17_12.predict.100rows.data.gz'
 
-        # 0-32 output class values
-        ### csvPathname = '../smalldata/hhp_50.short3.data'
+        if not os.path.exists(csvPathnamegz):
+            raise Exception("Can't find %s.gz" % (csvPathnamegz))
 
-        # 0-15 output class values
-        ## csvPathname = '../smalldata/hhp_107_01_short.data'
-
-        # don't want to worry about timeout on RF, so just take the first 100 rows
-        # csvPathname = '../smalldata/hhp_9_17_12.predict.data'
-        csvPathname = '../smalldata/hhp_9_17_12.predict.100rows.data'
-
-        if not os.path.exists(csvPathname) :
-            # check if the gz exists (git will have the .gz)
-            if os.path.exists(csvPathname + '.gz') :
-                # keep the .gz in place
-                h2o.spawn_cmd_and_wait(
-                    'gunzip', 
-                    ['gunzip', csvPathname + '.gz'], 
-                    timeout=4)
-            else:
-                raise Exception("Can't find %s or %s.gz" % (csvPathname, csvPathname))
-
-        cmd.runRF( trees=6, timeoutSecs=10,
-                csvPathname=csvPathname)
+        cmd.runRF(trees=6, timeoutSecs=10, csvPathname=csvPathnamegz)
 
 if __name__ == '__main__':
     h2o.clean_sandbox()
