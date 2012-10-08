d1ffb75bdb9ed57852de5d83dfadbbe31b7f7f19
Kevin Normoyle <kevin@0xdata.com>
2012-10-07 22:52:29 -0700

    build_remote_cloud in h2o.py. test_remote_cloud.py tests with hardwired hosts list

---
+++ py/test_remote_cloud.py
@@ -0,0 +1,65 @@
+import os, json, unittest, time, shutil, sys, time
+import h2o_cmd, h2o
+
+class Basic(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        global hosts
+        global nodes
+        global nodes_per_host
+
+        # we may have big delays with 2 jvms (os?)
+        # also: what is the agreement on json visible cloud state in each node, vs the paxos algorithm
+        # (timing)
+        nodes_per_host = 1
+        hosts = []
+        if (1==0):
+            hosts = [
+                h2o.RemoteHost('192.168.1.150', '0xdiag', '0xdiag'),
+                h2o.RemoteHost('192.168.1.151', '0xdiag', '0xdiag'),
+                h2o.RemoteHost('192.168.1.152', '0xdiag', '0xdiag'),
+                h2o.RemoteHost('192.168.1.153', '0xdiag', '0xdiag'),
+                h2o.RemoteHost('192.168.1.154', '0xdiag', '0xdiag'),
+                h2o.RemoteHost('192.168.1.155', '0xdiag', '0xdiag'),
+                h2o.RemoteHost('192.168.1.156', '0xdiag', '0xdiag'),
+                h2o.RemoteHost('192.168.1.160', '0xdiag', '0xdiag'),
+            ]
+        else:
+            hosts = [
+                h2o.RemoteHost('192.168.0.35', 'diag0x', 'diag0x'),
+                h2o.RemoteHost('192.168.0.37', 'diag0x', 'diag0x')
+            ]
+
+        h2o.upload_jar_to_remote_hosts(hosts)
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_remote_Cloud(self):
+        # FIX! too mfiles open at 240 iterations. file closes?
+        for i in range(1,100):
+            sys.stdout.write('.')
+            sys.stdout.flush()
+
+            nodes = h2o.build_remote_cloud(hosts, nodes_per_host, 
+                base_port=55321, ports_per_node=3)
+
+            for n in nodes:
+                print "Checking n:", n
+                c = n.get_cloud()
+                self.assertEqual(c['cloud_size'], len(nodes), 'inconsistent cloud size')
+
+            print "Tearing down cloud"
+            h2o.tear_down_cloud(nodes)
+
+
+if __name__ == '__main__':
+    h2o.unit_main()
+
