f5602c0401bb8a2ea9e7e17b1cce03ca3be2a24c
Michal Malohlava <michal.malohlava@gmail.com>
2012-10-09 21:05:00 +0200

    Fix of bug HTWO-144. Improved ParserTest - test different separators now.

--- test-src/test/ParserTest.java
+++ test-src/test/ParserTest.java
@@ -21,6 +21,7 @@ public class ParserTest {
   private double[] d(double... ds) { return ds; }
   private String[] s(String...ss)  { return ss; }
   private final double NaN = Double.NaN;
+  private final char[] SEPARATORS = new char[] {',', ' '};
 
   private Key[] k(String... data) {
     Key[] keys = new Key[data.length];
@@ -35,46 +36,61 @@ public class ParserTest {
   }
 
   @Test public void testBasic() {
-    Object[][] t = new Object[][] {
-        { "1,2,3",          d(1.0, 2.0, 3.0), },
-        { "4,5,6",          d(4.0, 5.0, 6.0), },
-        { "4,5.2,",         d(4.0, 5.2, NaN), },
-        { ",,",             d(NaN, NaN, NaN), },
-        { "asdf,qwer,1",    d(NaN, NaN, 1.0), },
-        { "1.1",            d(1.1, NaN, NaN), },
-        { "1.1,2.1,3.4",    d(1.1, 2.1, 3.4), },
+    String[] data = new String[] {
+        "1|2|3",
+        "4|5|6",
+        "4|5.2|",
+        "||",
+        "asdf|qwer|1",
+        "1.1",
+        "1.1|2.1|3.4",
     };
-    int i = 0;
-    SeparatedValueParser p;
 
-    StringBuilder sb = new StringBuilder();
-    for( i = 0; i < t.length; ++i ) sb.append(t[i][0]).append("\n");
+    double[][] exp = new double[][] {
+        d(1.0, 2.0, 3.0),
+        d(4.0, 5.0, 6.0),
+        d(4.0, 5.2, NaN),
+        d(NaN, NaN, NaN),
+        d(NaN, NaN, 1.0),
+        d(1.1, NaN, NaN),
+        d(1.1, 2.1, 3.4),
+    };
 
-    Key k = Key.make();
-    DKV.put(k, new Value(k, sb.toString()));
+    for (char separator : SEPARATORS) {
+      String[] dataset = getDataForSeparator(separator, data);
 
-    p = new SeparatedValueParser(k, ',', 3);
-    i = 0;
-    for( Row r : p ) {
-      Assert.assertArrayEquals((double[]) t[i++][1], r._fieldVals, 0.0001);
-    }
+      int i = 0;
+      SeparatedValueParser p;
 
-    sb = new StringBuilder();
-    for( i = 0; i < t.length; ++i ) sb.append(t[i][0]).append("\r\n");
-    DKV.put(k, new Value(k, sb.toString()));
+      StringBuilder sb = new StringBuilder();
+      for( i = 0; i < dataset.length; ++i ) sb.append(dataset[i]).append("\n");
 
-    p = new SeparatedValueParser(k, ',', 3);
-    i = 0;
-    for( Row r : p ) {
-      Assert.assertArrayEquals((double[]) t[i++][1], r._fieldVals, 0.0001);
+      Key k = Key.make();
+      DKV.put(k, new Value(k, sb.toString()));
+
+      p = new SeparatedValueParser(k, separator, 3);
+      i = 0;
+      for( Row r : p ) {
+        Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
+      }
+
+      sb = new StringBuilder();
+      for( i = 0; i < dataset.length; ++i ) sb.append(dataset[i]).append("\r\n");
+      DKV.put(k, new Value(k, sb.toString()));
+
+      p = new SeparatedValueParser(k, separator, 3);
+      i = 0;
+      for( Row r : p ) {
+        Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
+      }
     }
   }
 
   @Test public void testChunkBoundaries() {
     String[] data = new String[] {
-        "1,2", ",3\n",
-        "2,3,", "4\n",
-        "3,4,5"
+        "1|2", "|3\n",
+        "2|3|", "4\n",
+        "3|4|5"
     };
     double[][] exp = new double[][] {
         d(1.0, 2.0, 3.0),
@@ -82,57 +98,60 @@ public class ParserTest {
         d(3.0, 4.0, 5.0),
     };
 
-    Key[] keys = k(data);
+    for (char separator : SEPARATORS) {
+      String[] dataset = getDataForSeparator(separator, data);
+      Key[] keys = k(dataset);
 
-    int i = 0;
-    SeparatedValueParser p;
+      int i = 0;
+      SeparatedValueParser p;
 
-    p = new SeparatedValueParser(keys[0], ',', 3);
-    for( Row r : p ) {
-      Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
+      p = new SeparatedValueParser(keys[0], separator, 3);
+      for( Row r : p ) {
+        Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
+      }
+      Assert.assertEquals(1, i);
+      p = new SeparatedValueParser(keys[1], separator, 3);
+      for( Row r : p ) {
+        Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
+      }
+      Assert.assertEquals(2, i);
+      p = new SeparatedValueParser(keys[2], separator, 3);
+      for( Row r : p ) {
+        Assert.fail("Key 2 should have skipped the record: 2,3,4 got " + r);
+      }
+      Assert.assertEquals(2, i);
+      p = new SeparatedValueParser(keys[3], separator, 3);
+      for( Row r : p ) {
+        Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
+      }
+      Assert.assertEquals(3, i);
+      Assert.assertEquals(3, exp.length);
+      p = new SeparatedValueParser(keys[4], separator, 3);
+      for( Row r : p ) {
+        Assert.fail("Key 4 should have skipped the record: 3,4,5 got " + r);
+      }
+      Assert.assertEquals(5, keys.length);
     }
-    Assert.assertEquals(1, i);
-    p = new SeparatedValueParser(keys[1], ',', 3);
-    for( Row r : p ) {
-      Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
-    }
-    Assert.assertEquals(2, i);
-    p = new SeparatedValueParser(keys[2], ',', 3);
-    for( Row r : p ) {
-      Assert.fail("Key 2 should have skipped the record: 2,3,4 got " + r);
-    }
-    Assert.assertEquals(2, i);
-    p = new SeparatedValueParser(keys[3], ',', 3);
-    for( Row r : p ) {
-      Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
-    }
-    Assert.assertEquals(3, i);
-    Assert.assertEquals(3, exp.length);
-    p = new SeparatedValueParser(keys[4], ',', 3);
-    for( Row r : p ) {
-      Assert.fail("Key 4 should have skipped the record: 3,4,5 got " + r);
-    }
-    Assert.assertEquals(5, keys.length);
   }
 
   @Test public void testChunkBoundariesMixedLineEndings() {
     String[] data = new String[] {
-        "1,2", ",3\n",
-        "2,3,", "4\n",
-        "3,4,5\r\n",
-        "4,5,6", "\r\n",
-        "5", ",6,", "7\r\n",
-        "6,7,8\r\n" +
-        "7,8,9\r\n" +
-        "8,9", ",10\r\n" +
-        "9,10,11\n" +
-        "10,11,12",
-        "\n11,12,13", "\n" +
-        "12,13,14\n" +
-        "13,14,15\n" +
-        "14,15,16\r", "\n" +
-        "15,16,17\n" +
-        "16,17,18"
+        "1|2", "|3\n",
+        "2|3|", "4\n",
+        "3|4|5\r\n",
+        "4|5|6", "\r\n",
+        "5", "|6|", "7\r\n",
+        "6|7|8\r\n" +
+        "7|8|9\r\n" +
+        "8|9", "|10\r\n" +
+        "9|10|11\n" +
+        "10|11|12",
+        "\n11|12|13", "\n" +
+        "12|13|14\n" +
+        "13|14|15\n" +
+        "14|15|16\r", "\n" +
+        "15|16|17\n" +
+        "16|17|18"
     };
     double[][] exp = new double[][] {
         d(1,  2,  3),
@@ -153,28 +172,33 @@ public class ParserTest {
         d(16, 17, 18),
     };
 
-    Key[] keys = k(data);
+    for (char separator : SEPARATORS) {
+      String[] dataset = getDataForSeparator(separator, data);
 
-    int i = 0;
-    SeparatedValueParser p;
-    for( Key k : keys ) {
-      p = new SeparatedValueParser(k, ',', 3);
-      for( Row r : p ) {
-        Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
+      Key[] keys = k(dataset);
+
+      int i = 0;
+      SeparatedValueParser p;
+      for( Key k : keys ) {
+        p = new SeparatedValueParser(k, separator, 3);
+        for( Row r : p ) {
+          Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
+        }
       }
+      Assert.assertEquals(exp.length, i);
     }
-    Assert.assertEquals(exp.length, i);
   }
 
   @Test public void testNondecimalColumns() {
     String data[] = {
-        "1,  2,one\n",
-        "3,  4,two\n",
-        "5,  6,three\n",
-        "7,  8,one\n",
-        "9, 10,two\n",
-        "11,12,three\n",
-        "13,14,one\n",
+        "1|  2|one\n",
+        "3|  4|two\n",
+        "5|  6|three\n",
+        "7|  8|one\n",
+        "9| 10|two\n",
+        "11|12|three\n",
+        "13|14|one\n",
+//        "15|16|\"two\"\n",
     };
     double[][] expDouble = new double[][] {
         d(1,  2, NaN), // preserve order
@@ -184,6 +208,7 @@ public class ParserTest {
         d(9, 10, NaN),
         d(11,12, NaN),
         d(13,14, NaN),
+//        d(15,16, NaN),
     };
 
     String[][] expString = new String[][] {
@@ -194,38 +219,43 @@ public class ParserTest {
         s(null,null, "two"),
         s(null,null, "three"),
         s(null,null, "one"),
+//        s(null,null, "\"two\""),
     };
 
     String expDomain[] = s( "one", "two", "three" );
-    Key[] keys = k(data);
 
-    SeparatedValueParser p;
-    ColumnDomain cds[] = new ColumnDomain[3];
-    for (int j = 0; j < 3; j++) cds[j] = new ColumnDomain();
-    int i = 0;
-    for( Key k : keys ) {
-      p = new SeparatedValueParser(k, ',', 3, cds);
-      for( Row r : p ) {
-        Assert.assertArrayEquals(expDouble[i], r._fieldVals, 0.0001);
-        Assert.assertArrayEquals(expString[i], r._fieldStringVals);
-        i++;
+    for (char separator : SEPARATORS) {
+      String[] dataset = getDataForSeparator(separator, data);
+      Key[] keys = k(dataset);
+
+      SeparatedValueParser p;
+      ColumnDomain cds[] = new ColumnDomain[3];
+      for (int j = 0; j < 3; j++) cds[j] = new ColumnDomain();
+      int i = 0;
+      for( Key k : keys ) {
+        p = new SeparatedValueParser(k, separator, 3, cds);
+        for( Row r : p ) {
+          Assert.assertArrayEquals(expDouble[i], r._fieldVals, 0.0001);
+          Assert.assertArrayEquals(expString[i], r._fieldStringVals);
+          i++;
+        }
       }
+      Assert.assertTrue ( cds[0].size() == 0 );
+      Assert.assertTrue ( cds[1].size() == 0 );
+      Assert.assertTrue ( cds[2].size() != 0 );
+      Assert.assertArrayEquals(expDomain, cds[2].toArray());
     }
-    Assert.assertTrue ( cds[0].size() == 0 );
-    Assert.assertTrue ( cds[1].size() == 0 );
-    Assert.assertTrue ( cds[2].size() != 0 );
-    Assert.assertArrayEquals(expDomain, cds[2].toArray());
   }
 
   @Test public void testMultipleNondecimalColumns() {
     String data[] = {
-        "foo,    2,one\n",
-        "bar,    4,two\n",
-        "foo,    6,three\n",
-        "bar,    8,one\n",
-        "bar,ten,two\n",
-        "bar,   12,three\n",
-        "foobar,14,one\n",
+        "foo|    2|one\n",
+        "bar|    4|two\n",
+        "foo|    6|three\n",
+        "bar|    8|one\n",
+        "bar|ten|two\n",
+        "bar|   12|three\n",
+        "foobar|14|one\n",
     };
     double[][] expDouble = new double[][] {
         d(NaN,   2, NaN), // preserve order
@@ -253,22 +283,79 @@ public class ParserTest {
         s( "one", "two", "three" ),
     };
 
-    Key[] keys = k(data);
+    for (char separator : SEPARATORS) {
+      String[] dataset = getDataForSeparator(separator, data);
+      Key[]    keys = k(dataset);
 
-    SeparatedValueParser p;
-    ColumnDomain cds[] = new ColumnDomain[3];
-    for (int j = 0; j < 3; j++) cds[j] = new ColumnDomain();
-    int i = 0;
-    for( Key k : keys ) {
-      p = new SeparatedValueParser(k, ',', 3, cds);
+      SeparatedValueParser p;
+      ColumnDomain cds[] = new ColumnDomain[3];
+      for (int j = 0; j < 3; j++) cds[j] = new ColumnDomain();
+      int i = 0;
+      for( Key k : keys ) {
+        p = new SeparatedValueParser(k, separator, 3, cds);
+        for( Row r : p ) {
+          Assert.assertArrayEquals(expDouble[i], r._fieldVals, 0.0001);
+          Assert.assertArrayEquals(expString[i], r._fieldStringVals);
+          i++;
+        }
+      }
+
+      for (int j = 0; j < 3; j++)
+        Assert.assertArrayEquals(expDomain[j], cds[j].toArray());
+    }
+  }
+
+
+
+  @Test public void testBasicSpaceAsSeparator() {
+
+    String[] data = new String[] {
+        "   1|2|3",
+        "  4  |   5  |     6",
+        "4|5.2 ",
+        " ",
+        "asdf|qwer|1",
+        "1.1",
+        "1.1|2.1|3.4",
+    };
+    double[][] exp = new double[][] {
+        d(1.0, 2.0, 3.0),
+        d(4.0, 5.0, 6.0),
+        d(4.0, 5.2, NaN),
+        d(NaN, NaN, NaN),
+        d(NaN, NaN, 1.0),
+        d(1.1, NaN, NaN),
+        d(1.1, 2.1, 3.4),
+    };
+
+    for (char separator : SEPARATORS) {
+      String[] dataset = getDataForSeparator(separator, data);
+
+      int i = 0;
+      StringBuilder sb = new StringBuilder();
+      for( i = 0; i < dataset.length; ++i ) sb.append(dataset[i]).append("\n");
+
+      Key k = Key.make();
+      DKV.put(k, new Value(k, sb.toString()));
+
+      SeparatedValueParser p;
+
+      i = 0;
+      p = new SeparatedValueParser(k, separator, 3);
       for( Row r : p ) {
-        Assert.assertArrayEquals(expDouble[i], r._fieldVals, 0.0001);
-        Assert.assertArrayEquals(expString[i], r._fieldStringVals);
-        i++;
+        Assert.assertArrayEquals(exp[i++], r._fieldVals, 0.0001);
       }
     }
+  }
 
-    for (int j = 0; j < 3; j++)
-      Assert.assertArrayEquals(expDomain[j], cds[j].toArray());
+  String[] getDataForSeparator(char sep, String[] data) {
+    return getDataForSeparator('|', sep, data);
+  }
+  String[] getDataForSeparator(char placeholder, char sep, String[] data) {
+    String[] result = new String[data.length];
+    for (int i = 0; i < data.length; i++) {
+      result[i] = data[i].replace(placeholder, sep);
+    }
+    return result;
   }
 }
