f685880ce595642cde10e1f2f96b64ad8108c6b4
Michal Malohlava <michal.malohlava@gmail.com>
2012-10-11 16:27:07 +0200

    Parsing improvements:  - improved column names guesser  - more python tests of parser

--- py/test_inspect.py
+++ py/test_inspect.py
@@ -5,6 +5,10 @@ import itertools
 def file_to_put():
     return 'smalldata/poker/poker1000'
 
+def crange(start, end):
+    for c in xrange(ord(start), ord(end)):
+        yield chr(c)
+
 # Dummy wc -l
 def wcl(filename):
         lines = 0
@@ -23,7 +27,6 @@ class Basic(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         h2o.tear_down_cloud()
-        #pass
 
     def setUp(self):
         pass
@@ -37,11 +40,39 @@ class Basic(unittest.TestCase):
         
         res  = self.putfile_and_parse(node, cvsfile)
         ary  = node.inspect(res['keyHref'])
-        # count lines in input file
+        # count lines in input file - there is no header for poker 1000
         rows = wcl(cvsfile)
 
         self.assertEqual(rows, ary['rows'])
         self.assertEqual(11, ary['cols'])
+
+    def test_B_inspect_column_names_multi_space_sep(self):
+        self.inspect_column_names("smalldata/test/test_26cols_multi_space_sep.csv")
+
+    def test_C_inspect_column_names_single_space_sep(self):
+        self.inspect_column_names("smalldata/test/test_26cols_single_space_sep.csv")
+
+    def test_D_inspect_column_names_comma_sep(self):
+        self.inspect_column_names("smalldata/test/test_26cols_comma_sep.csv")
+
+    def test_E_inspect_column_names_comma_sep(self):
+        self.inspect_column_names("smalldata/test/test_26cols_single_space_sep_2.csv")
+
+
+    # Shared test implementation for smalldata/test/test_26cols_*.csv
+    def inspect_column_names(self, filename):
+        cvsfile = h2o.find_file(filename)
+        node    = h2o.nodes[0]
+        
+        res  = self.putfile_and_parse(node, cvsfile)
+        ary  = node.inspect(res['keyHref'])
+
+        self.assertEqual(1, ary['rows'])
+        self.assertEqual(26, ary['cols'])
+
+        for (col, expName) in zip(ary['columns'], crange('A', 'Z')):
+            #h2o.verboseprint(expName, col['name'])            
+            self.assertEqual(expName, col['name'])
 
     def putfile_and_parse(self, node, f):
         result  = node.put_file(f)
