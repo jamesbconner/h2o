8259a4746d53d29db947a45a55b07039a52bce3c
Kevin Normoyle <kevin@0xdata.com>
2012-10-12 15:48:12 -0700

    latest version of this

--- py/test_remote_cloud.py
+++ py/test_remote_cloud.py
@@ -10,7 +10,7 @@ class Basic(unittest.TestCase):
 
         # we may have big delays with 2 jvms (os?)
         # also: what is the agreement on json visible cloud state in each node vs the paxos algorithm
-        node_count = 1
+        node_count = 4
         hosts = []
         # FIX! probably will just add args for -matt -kevin -0xdata that select different lists?
         # or we could have a hosts file that's local that you modify. just as easy to mod this though?
@@ -24,7 +24,7 @@ class Basic(unittest.TestCase):
                 h2o.RemoteHost('192.168.1.151', '0xdiag', '0xdiag'),
                 h2o.RemoteHost('192.168.1.152', '0xdiag', '0xdiag')
             ]
-        elif (1==1):
+        elif (1==0):
 
             hosts = [
                 h2o.RemoteHost('192.168.1.150', '0xdiag', '0xdiag'),
@@ -35,11 +35,16 @@ class Basic(unittest.TestCase):
                 h2o.RemoteHost('192.168.1.155', '0xdiag', '0xdiag'),
                 h2o.RemoteHost('192.168.1.156', '0xdiag', '0xdiag'),
                 h2o.RemoteHost('192.168.1.160', '0xdiag', '0xdiag'),
+                h2o.RemoteHost('192.168.1.161', '0xdiag', '0xdiag')
             ]
-        else:
+        elif (1==0) :
             hosts = [
                 h2o.RemoteHost('192.168.0.35', 'diag0x', 'diag0x'),
                 h2o.RemoteHost('192.168.0.37', 'diag0x', 'diag0x')
+            ]
+        else:
+            hosts = [
+                h2o.RemoteHost('192.168.1.160', '0xdiag', '0xdiag')
             ]
 
         h2o.upload_jar_to_remote_hosts(hosts)
@@ -55,7 +60,6 @@ class Basic(unittest.TestCase):
         pass
 
     def test_remote_Cloud(self):
-        # FIX! too mfiles open at 240 iterations. file closes?
         trial = 0
         for i in range(1,100):
             sys.stdout.write('.')
@@ -67,8 +71,10 @@ class Basic(unittest.TestCase):
             # FIX! if node[0] is fast, maybe the other nodes aren't at a point where they won't get
             # connection errors. Stabilize them too! Can have short timeout here, because they should be 
             # stable?? or close??
-            for n in nodes:
-                h2o.stabilize_cloud(n, len(nodes), timeoutSecs=3, retryDelaySecs=0.25)
+
+            # UPDATED: using "consensus" in node[0] should mean this is unnecessary?
+            # for n in nodes:
+            #    h2o.stabilize_cloud(n, len(nodes), timeoutSecs=3, retryDelaySecs=0.25)
 
             # now double check ...no stabilize tolerance of connection errors here
             for n in nodes:
@@ -78,12 +84,14 @@ class Basic(unittest.TestCase):
                 self.assertEqual(c['cloud_size'], len(nodes), 'inconsistent cloud size')
 
             trial += 1
+            # FIX! is this really needed? does tear_down_cloud do shutdown command and wait?
+            print "Sending shutdown to cloud, trial", trial
+            h2o.nodes[0].shutdown_all()
+            time.sleep(1)
+
             print "Tearing down cloud, trial", trial
             h2o.tear_down_cloud(nodes)
             h2o.clean_sandbox()
-            # kbn. temp hack ..wait for sticky open ports to close? (since we reuse the ports)
-            # time.sleep(4)
-
 
 if __name__ == '__main__':
     h2o.unit_main()
