d94912736b33111464360f39b73cc268eda28bea
Matt Fowles <matt.fowles@gmail.com>
2012-10-24 14:05:46 -0400

    Push DRF inhale to be more parallel

--- src/hex/rf/Data.java
+++ src/hex/rf/Data.java
@@ -39,13 +39,13 @@ public class Data implements Iterable<Row> {
   protected Data(DataAdapter da) { data_ = da; }
 
   protected int start()          { return 0;                   }
-  protected int end()            { return data_.rows();        }
+  protected int end()            { return data_._numRows;        }
   public int rows()              { return end() - start();     }
   public int columns()           { return data_.columns();     }
   public int classes()           { return data_.classes();     }
   public int seed()              { return data_.seed();        }
   public int dataId()            { return data_.dataId();      }
-  public int classIdx()          { return data_.classIdx_;     }
+  public int classIdx()          { return data_._classIdx;     }
   public String colName(int i)   { return data_.columnNames()[i]; }
   public float unmap(int col, float split) { return data_.unmap(col, split); }
   public int columnArity(int colIndex) { return data_.columnArity(colIndex); }
--- src/hex/rf/DataAdapter.java
+++ src/hex/rf/DataAdapter.java
@@ -1,24 +1,23 @@
 package hex.rf;
 
-import gnu.trove.map.hash.TFloatIntHashMap;
-import gnu.trove.map.hash.TFloatShortHashMap;
+import gnu.trove.map.hash.*;
 
 import java.text.DecimalFormat;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
+
+import jsr166y.RecursiveAction;
 
 class DataAdapter  {
-  private short[] data_;
-  private C[] c_;
-  private HashMap<String, Integer> c2i_ = new HashMap<String,Integer>();
-  private String name_="";
-  public  int classIdx_;
-  private final int _data_id;           // Unique cookie identifying this dataset
-  private boolean frozen_;
-  private int numClasses_=-1;
-  private String[] columnNames_;
+  private short[] _data;
+  private C[] _c;
+  private TObjectIntHashMap<String> _c2i = new TObjectIntHashMap<String>();
+  private final String _name;
+  private final int _dataId;           // Unique cookie identifying this dataset
+  private int _numClasses;
+  private String[] _columnNames;
   private final int _seed;
-  private int rows;
+  public final int _classIdx;
+  public final int _numRows;
 
   /** Create a data adapter
    * @param name  of the dataset
@@ -31,115 +30,121 @@ class DataAdapter  {
    */
   DataAdapter(String name, Object[] columns, String classNm, int[] ignores, int rows, int data_id, int seed, int numClasses) {
     _seed = seed+data_id;
-    name_=name;
-    c_ = new C[columns.length];
-    columnNames_ = new String[columns.length];
+    _name=name;
+    _c = new C[columns.length];
+    _columnNames = new String[columns.length];
     // Note that the number of classes is not generally known in a distributed
     // fashion, as any single JVM does not see all the data - so it needs to be
     // passed in here.
-    numClasses_ = numClasses;
+    _numClasses = numClasses;
     for( int i=0; i<columns.length; i++ ) {
-      String s=columns[i].toString();  columnNames_[i] = s; c2i_.put(s,i);
+      _c2i.put(_columnNames[i] = columns[i].toString(), i);
     }
-    classIdx_ = c2i_.get(classNm);
+    _classIdx = _c2i.get(classNm);
     assert ignores.length < columns.length;
     for( int i=0; i<columns.length; i++ ) {
       boolean ignore = false;
       for(int j=0;j<ignores.length;j++) if(ignores[j]==i) ignore=true;
-      c_[i]= new C(columnNames_[i],rows, i==classIdx_,ignore);
+      _c[i]= new C(_columnNames[i], rows, i==_classIdx,ignore);
     }
-    _data_id = data_id;
+    _dataId = data_id;
+    _numRows = rows;
   }
 
   /** Given a value in enum format, returns a value in the original range. */
   public float unmap(int col, float v){  // FIXME should this be a short???? JAN
     short idx = (short)v; // Convert split-point of the form X.5 to a (short)X
-    C c = c_[col];
+    C c = _c[col];
     if (v == idx) {  // this value isn't a split
       return c._v2o[idx+0];
     } else {
       float flo = c._v2o[idx+0]; // Convert to the original values
-      float fhi = (idx < rows()) ? c._v2o[idx+1] : flo+1.0f;
+      float fhi = (idx < _numRows) ? c._v2o[idx+1] : flo+1.0f;
       float fmid = (flo+fhi)/2.0f; // Compute an original split-value
       assert flo < fmid && fmid < fhi; // Assert that the float will properly split
       return fmid;
     }
   }
   /** Return the name of the data set. */
-  public String name() { return name_; }
+  public String name() { return _name; }
 
   /** Encode the data in a compact form.*/
-  public void shrinkWrap() {
+  public ArrayList<RecursiveAction> shrinkWrap() {
     freeze();
+    ArrayList<RecursiveAction> res = new ArrayList(_c.length);
     // Note: currently we are allocating space for all columns, including the ones we ignore.
     // Changing this would reduce footprint.
-    short[][] vss = new short[c_.length][];
-    for( int i=0; i<c_.length; i++ )   vss[i] = c_[i].shrink();
-    data_ = new short[ c_.length * rows()];
-    for(int i=0;i<c_.length;i++) {
-      if (ignore(i)) continue; short[] vs = vss[i]; for(int j=0;j<vs.length;j++) setS(j,i,vs[j]);
+    for(int i=0;i<_c.length;i++) {
+      if( ignore(i) ) continue;
+      final int col = i;
+      res.add(new RecursiveAction() {
+        protected void compute() {
+          short[] vs = _c[col].shrink();
+          for(int j = 0; j < vs.length; j++) setS(j, col, vs[j]);
+        };
+      });
     }
+    return res;
   }
 
-  public int  seed()          { return _seed; }
-  public void freeze()        { frozen_=true; }
-  public int columns()        { return c_.length;}
-  public int rows()           { return rows; }
-  public int classOf(int idx) { return getS(idx,classIdx_); }
-  public int dataId()         { return _data_id; }
+  public void freeze()        { _data = new short[ _c.length * _numRows]; }
+  public int seed()           { return _seed; }
+  public int columns()        { return _c.length;}
+  public int classOf(int idx) { return getS(idx,_classIdx); }
+  public int dataId()         { return _dataId; }
   /** The number of possible prediction classes. */
-  public int classes()        { return numClasses_; }
+  public int classes()        { return _numClasses; }
   /** True if we should ignore column i. */
-  public boolean ignore(int i)     { return c_[i].ignore(); }
+  public boolean ignore(int i)     { return _c[i].ignore(); }
 
   /** Returns the number of bins, i.e. the number of distinct values in the column.  Zero if we are ignoring the column. */
-  public int columnArity(int col) { if (ignore(col)) return 0; else return c_[col].smax_; }
+  public int columnArity(int col) { if (ignore(col)) return 0; else return _c[col]._smax; }
   /** Return a short that represents the binned value of the original row,column value.  */
   public short getEncodedColumnValue(int rowIndex, int colIndex) { return getS(rowIndex, colIndex); }
 
   /** Return the array of all column names including ignored and class. */
-  public String[] columnNames() { return columnNames_; }
+  public String[] columnNames() { return _columnNames; }
 
   /** Add a row to this data set. */
-  public void addRow(float[] v) {
-    if (frozen_) throw new Error("Frozen data set update");
-    for(int i=0;i<v.length;i++)  c_[i].add(v[i]);
-    rows++;
+  public void addRow(float[] v, int row) {
+    if( _data != null ) throw new Error("Frozen data set update");
+    for( int i = 0; i < v.length; i++ ) _c[i].add(v[i], row);
   }
-  short getS(int row, int col) { return data_[row * c_.length + col]; }
-  void setS(int row, int col, short val) { data_[row * c_.length + col]= val; }
-  protected float getF(int col, int idx) { return c_[col].getF(idx); }
+  short getS(int row, int col) { return _data[row * _c.length + col]; }
+  void setS(int row, int col, short val) { _data[row * _c.length + col]= val; }
+  protected float getF(int col, int idx) { return _c[col].getF(idx); }
   static final DecimalFormat df = new  DecimalFormat ("0.##");
 
-  private class C {
-    String name_;
+  private static class C {
+    String _name;
     boolean _ignore, _isClass;
-    float min_=Float.MAX_VALUE, max_=Float.MIN_VALUE, tot_;
-    float[] v_;
-
+    float _min=Float.MAX_VALUE, _max=Float.MIN_VALUE, _tot;
+    float[] _v;
     float[] _v2o;  // Reverse (short) indices to original floats
-    short smax_ = -1;
+    short _smax = -1;
 
     C(String s, int rows, boolean isClass, boolean ignore) {
-      name_ = s; v_ = new float[rows]; _isClass=isClass; _ignore=ignore;
+      _name = s;
+      _v = new float[rows];
+      _isClass = isClass;
+      _ignore = ignore;
     }
 
-    void add(float x) {
-      min_=Math.min(x,min_);
-      max_=Math.max(x,max_);
-      tot_+=x;
-      v_[rows()]=x;
+    void add(float x, int row) {
+      _min=Math.min(x,_min);
+      _max=Math.max(x,_max);
+      _tot+=x;
+      _v[row]=x;
     }
 
-    float getF(int i) { return v_[i]; }
+    float getF(int i) { return _v[i]; }
     boolean ignore() { return _ignore; }
 
-
     public String toString() {
-      String res = "col("+name_+")";
+      String res = "col("+_name+")";
       if (ignore()) return res + " ignored!";
-      res+= "  ["+DataAdapter.df.format(min_) +","+DataAdapter.df.format(max_)+"], avg=";
-      res+= DataAdapter.df.format(tot_/rows()) ;
+      res+= "  ["+DataAdapter.df.format(_min) +","+DataAdapter.df.format(_max)+"], avg=";
+      res+= DataAdapter.df.format(_tot/_v.length) ;
       if (_isClass) res+= " CLASS ";
       return res;
     }
@@ -150,16 +155,16 @@ class DataAdapter  {
         it's one-based (e.g. covtype) or -1/+1 (arcene).   */
     short[] shrink() {
       if (ignore()) return null;
-      short[] res = new short[rows()];
+      short[] res = new short[_v.length];
       if( _isClass ) {
-        for(float v : v_) smax_ = v > smax_ ? (short) v : smax_;
-        int min = (int)min_;
-        for( int j = 0; j < rows(); j++ ) res[j] = (short)((int)v_[j]-min);
+        for(float v : _v) _smax = v > _smax ? (short) v : _smax;
+        int min = (int)_min;
+        for( int j = 0; j < _v.length; j++ ) res[j] = (short)((int)_v[j]-min);
       } else {
         TFloatShortHashMap o2v2 = hashCol();
-        for( int j = 0; j < rows(); j++ ) res[j] = o2v2.get(v_[j]);
+        for( int j = 0; j < _v.length; j++ ) res[j] = o2v2.get(_v[j]);
       }
-      v_= null;
+      _v= null;
       return res;
     }
 
@@ -169,10 +174,10 @@ class DataAdapter  {
     TFloatShortHashMap hashCol() {
       // Remove duplicate floats
       TFloatIntHashMap freq = new TFloatIntHashMap(100);
-      for( int i = 0; i < rows(); i++ ) freq.adjustOrPutValue(v_[i], 1, 1);
+      for( int i = 0; i < _v.length; i++ ) freq.adjustOrPutValue(_v[i], 1, 1);
 
       // Compute bin-size
-      int bin_size = (freq.size() > BIN_LIMIT) ? (rows() / BIN_LIMIT) : 1;
+      int bin_size = (freq.size() > BIN_LIMIT) ? (_v.length / BIN_LIMIT) : 1;
       if( bin_size > 1 )
         Utils.pln(this + " this column's arity was cut from "+ freq.size()+ " to " + BIN_LIMIT);
 
@@ -182,14 +187,14 @@ class DataAdapter  {
       // Assign shorts to floats, with binning.
       _v2o = new float[ks.length]; // Reverse mapping
       TFloatShortHashMap res = new TFloatShortHashMap();
-      smax_ = 0;
+      _smax = 0;
       int bin_cnt = 0;
       for( float d : ks ) {
-        _v2o[smax_] = d;           // Reverse mapping
-        res.put(d, smax_);         // Forward mapping
+        _v2o[_smax] = d;           // Reverse mapping
+        res.put(d, _smax);         // Forward mapping
         bin_cnt += freq.get(d);    // Grow bin
         if( bin_cnt > bin_size ) { // This bin is full?
-          smax_++;                 // Flip bins!
+          _smax++;                 // Flip bins!
           bin_cnt=0;
         }
       }
