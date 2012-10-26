5486eb14772d1be14b3cf88a1d4516508100e36a
Tomas Nykodym <tomas.nykodym@gmail.com>
2012-10-26 11:37:23 -0700

    NOP task update. Records missing keys.

--- src/hex/NOPTask.java
+++ src/hex/NOPTask.java
@@ -5,11 +5,22 @@ import water.*;
 public class NOPTask extends MRTask {
 
   public int _res;
+  public int _missingKeysCount;
+  public int _minMissingKey = Integer.MAX_VALUE;
+  public int _maxMissingKey = Integer.MIN_VALUE;
+
   @Override
   public void map(Key key) {
-    byte [] mem = DKV.get(key).get();
-    for(byte b:mem){
-      _res ^= b;
+    Value v = DKV.get(key);
+    if(v != null){
+      byte [] mem = DKV.get(key).get();
+      for(byte b:mem){
+        _res ^= b;
+      }
+    } else {
+      ++_missingKeysCount;
+      _minMissingKey = ValueArray.getChunkIndex(key);
+      _maxMissingKey = _minMissingKey;
     }
   }
 
@@ -17,5 +28,14 @@ public class NOPTask extends MRTask {
   public void reduce(DRemoteTask drt) {
     NOPTask other = (NOPTask)drt;
     _res ^= other._res;
+    _missingKeysCount += other._missingKeysCount;
+    _minMissingKey = Math.min(_minMissingKey, other._minMissingKey);
+    _maxMissingKey = Math.max(_maxMissingKey, other._maxMissingKey);
+  }
+
+  public String toString(){
+    if(_missingKeysCount > 0)
+      return _missingKeysCount +  " keys missing,  Min = " + _minMissingKey + ", Max = " + _maxMissingKey + ", byte xor = " + _res;
+    return "No keys were missing. Byte xor = "  + _res;
   }
 }
