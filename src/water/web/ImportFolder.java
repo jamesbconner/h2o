e64ae7d9c37c674897d8c858861b307353126b2b
thepeta <thepeta@0xdata.com>
2012-10-29 15:26:34 -0700

    ImportFolder now makes sure that all nodes agree on the file sizes for the imported files.

--- src/water/web/ImportFolder.java
+++ src/water/web/ImportFolder.java
@@ -1,53 +1,202 @@
 package water.web;
 
+import hex.Models;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import water.*;
 
 public class ImportFolder extends H2OPage {
+  
+  
+  public static class FolderIntegrityChecker extends DRemoteTask {
 
-  int importFile(File f, byte rf, int num, RString response) {
-    if( f.isDirectory() ) {
-      for( File f2 : f.listFiles() )
-        num = importFile(f2,rf,num,response);
-      return num;
+    String[] _files;
+    long[] _sizes;
+    short[] _ok;
+    int _nodes;
+    int _stringSizes;
+    
+    ArrayList<File> _filesInProgress;
+    
+    @Override public void compute() {
+      _ok = new short[_files.length];
+      for (int i = 0; i < _files.length; ++i) {
+        File f = new File(_files[i]);
+        if (f.exists() && (f.length()==_sizes[i]))
+          _ok[i] = 1;
+      }
+      ++_nodes;
+      tryComplete();
     }
 
-    RString row = response.restartGroup("entry");
-    Key k = PersistNFS.decodeFile(f);
-    String fname = f.getName();
-    long size = f.length();
-    Value val = (size < 2*ValueArray.chunk_size())
-      ? new Value((int)size,0,k,Value.NFS)
-      : new ValueArray(k,size,Value.NFS);
-    val.setdsk();
-    UKV.put(k,val);
-    row.replace("contents",success("File "+fname+" imported as key <strong>"+k+"</strong>"));
-    row.append();
-    return num+1;
+    @Override public void reduce(DRemoteTask drt) {
+      FolderIntegrityChecker other = (FolderIntegrityChecker) drt;
+      if (_ok == null)
+        _ok = other._ok;
+      else
+        for (int i = 0; i < _ok.length; ++i)
+          _ok[i] += other._ok[i];
+      _nodes += other._nodes;
+    }
+
+    private void addFolder(File folder) {
+      if (folder.isDirectory()) {
+        for (File f: folder.listFiles()) {
+          if (f.isDirectory())
+            addFolder(f);
+          else
+            _filesInProgress.add(f);
+        }
+      } else {
+        _filesInProgress.add(folder);
+      }
+    }
+    
+    
+    public FolderIntegrityChecker(File root) {
+      _filesInProgress = new ArrayList();
+      addFolder(root);
+      _files = new String[_filesInProgress.size()];
+      _sizes = new long[_filesInProgress.size()];
+      _stringSizes = 0;
+      for (int i = 0; i < _files.length; ++i) {
+        File f = _filesInProgress.get(i);
+        _files[i] = f.getAbsolutePath();
+        _sizes[i] = f.length();
+        _stringSizes += _files[i].length();
+      }
+      _filesInProgress = null;
+      _nodes = 0;
+    }
+    
+    private Key importFile(int i) {
+      if (_ok[i] == _nodes) {
+        File f = new File(_files[i]);
+        Key k = PersistNFS.decodeFile(f);
+        String fname = f.getName();
+        long size = f.length();
+        Value val = (size < 2*ValueArray.chunk_size())
+          ? new Value((int)size,0,k,Value.NFS)
+          : new ValueArray(k,size,Value.NFS);
+        val.setdsk();
+        UKV.put(k,val);
+        return k;
+      } else {
+        return null;
+      }
+    }
+    
+    public String importFilesHTML() {
+      StringBuilder sb = new StringBuilder();
+      int correct = 0;
+      for (int i = 0; i < _files.length; ++i) {
+        Key k = importFile(i);
+        if (k == null) {
+          sb.append(error("File <strong>"+_files[i]+"</strong> does not have the same size on all nodes."));
+        } else {
+          sb.append(success("File <strong>"+_files[i]+"</strong> imported as key <strong>"+k.toString()+"</strong>"));
+          ++correct;
+        }
+      }
+      return "Out of "+_files.length+" a total of "+correct+" was successfully imported to the cloud."+sb.toString(); 
+    }
+    
+    public FolderIntegrityChecker() {
+      
+    }
+    
+    @Override public int wire_len() { 
+      return super.wire_len() + 4 + 4 + 4 +_stringSizes + _files.length*(2+8+2);
+    }
+
+    @Override public void write(DataOutputStream os) throws IOException {
+      super.write(os);
+      os.writeInt(_files.length);
+      os.writeInt(_stringSizes);
+      os.writeInt(_nodes);
+      for (int i = 0; i < _files.length; ++i) {
+        os.writeShort(_files[i].length());
+        os.write(_files[i].getBytes());
+        os.writeLong(_sizes[i]);
+        os.writeShort(_ok == null ? 0 : _ok[i]);
+      }
+    }
+
+    @Override public void write(Stream s) {
+      super.write(s);
+      s.set4(_files.length);
+      s.set4(_stringSizes);
+      s.set4(_nodes);
+      for (int i = 0; i < _files.length; ++i) {
+        s.set2(_files[i].length());
+        s.setBytes(_files[i].getBytes(),_files[i].length());
+        s.set8(_sizes[i]);
+        s.set2(_ok == null ? 0 : _ok[i]);
+      }
+    }
+
+    @Override public void read(DataInputStream is) throws IOException {
+      super.read(is);
+      int fLength = is.readInt();
+      _stringSizes = is.readInt();
+      _nodes = is.readInt();
+      _files = new String[fLength];
+      _sizes = new long[fLength];
+      _ok = new short[fLength];
+      byte[] buffer = new byte[1000];
+      for (int i = 0; i < fLength; ++i) {
+        int ss = is.readShort();
+        if (ss>buffer.length)
+          buffer = new byte[ss];
+        is.read(buffer, 0, ss);
+        _files[i] = new String(buffer, 0, ss);
+        _sizes[i] = is.readLong();
+        _ok[i] = is.readShort();
+      }
+    }
+  
+    @Override public void read(Stream s) {
+      super.read(s);
+      int fLength = s.get4();
+      _stringSizes = s.get4();
+      _nodes = s.get4();
+      _files = new String[fLength];
+      _sizes = new long[fLength];
+      _ok = new short[fLength];
+      byte[] buffer = new byte[1000];
+      for (int i = 0; i < fLength; ++i) {
+        int ss = s.get2();
+        if (ss>buffer.length)
+          buffer = new byte[ss];
+        s.getBytes(buffer,ss);
+        _files[i] = new String(buffer, 0, ss);
+        _sizes[i] = s.get8();
+        _ok[i] = (short) s.get2();
+      }
+    }
   }
 
   @Override protected String serveImpl(Server server, Properties args, String sessionID) {
-    String folder = args.getProperty("Folder");
-    int rf = getAsNumber(args, "RF", Key.DEFAULT_DESIRED_REPLICA_FACTOR);
-    if ((rf<0) || (rf>127))
-      return error("Replication factor must be from 0 to 127.");
-
-    int num = 0;
-    RString response = new RString(html);
     try {
-      File root = new File(folder).getCanonicalFile();
-      if( !root.exists() ) throw new IOException(root+" not found");
-      num = importFile(root,(byte)rf,0,response);
-    } catch (IOException e) {
-      RString row = response.restartGroup("entry");
-      row.replace("contents",error("Unable to import file <strong>"+folder+"</strong>:")+"<pre>"+e.toString()+"</pre>");
-      row.append();
+      String folder = args.getProperty("Folder");
+      int rf = getAsNumber(args, "RF", Key.DEFAULT_DESIRED_REPLICA_FACTOR);
+      if ((rf<0) || (rf>127))
+        throw new Exception("Replication factor must be from 0 to 127.");
+
+      File root = new File(folder);
+      if (!root.exists())
+        throw new Exception("Unable to import folder "+folder+". Folder not found.");
+      FolderIntegrityChecker checker = new FolderIntegrityChecker(new File(folder));
+      checker.invokeOnAllNodes();
+      return checker.importFilesHTML();
+    } catch (Exception e) {
+      return error(e.toString());
     }
-    response.replace("num",num);
-    return response.toString();
   }
 
   @Override public String[] requiredArguments() {
