1d47870a1190a425e914e6904f9581fd06faffb5
Tomas Nykodym <tomas.nykodym@gmail.com>
2012-10-30 14:46:32 -0700

    Added stub of FastTrie.

---
+++ src/water/parser/FastTrie.java
@@ -0,0 +1,27 @@
+package water.parser;
+
+/**
+ * Trie for parsing enums in the FastParser.
+ *
+ *
+ * @author tomasnykodym
+ *
+ */
+public final class FastTrie {
+
+  void addChar(char c){
+
+  }
+
+  /**
+   * get token id of the currentlyparsed String and reset the Trie to start new word.
+   */
+  public int getTokenId(){
+    return 0;
+  }
+
+  public void update(FastTrie other){
+
+  }
+
+}
