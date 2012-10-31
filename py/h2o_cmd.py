5ed24f4891abba3e3812d53bd6771145e5b1ec41
Kevin Normoyle <kevin@0xdata.com>
2012-10-31 14:28:53 -0700

    key returned by Parse.json is now called "Key"..changed from "key"

--- py/h2o_cmd.py
+++ py/h2o_cmd.py
@@ -14,7 +14,7 @@ def runGLMOnly(node=None,parseKey=None,X="0",Y="1",timeoutSecs=30,retryDelaySecs
     if not parseKey: raise Exception('No file name for GLM specified')
     if not node: node = h2o.nodes[0]
     # FIX! add something like stabilize in RF to check results, and also retry/timeout
-    glm = node.GLM(parseKey['key'],X=X,Y=Y)
+    glm = node.GLM(parseKey['Key'],X=X,Y=Y)
     return glm
 
 # You can change those on the URL line woth "&colA=77&colB=99"
@@ -33,7 +33,7 @@ def runLROnly(node=None,parseKey=None,colA=0,colB=1,timeoutSecs=30,retryDelaySec
     if not parseKey: raise Exception('No file name for LR specified')
     if not node: node = h2o.nodes[0]
     # FIX! add something like stabilize in RF to check results, and also retry/timeout
-    lr = node.linear_reg(parseKey['key'], colA, colB)
+    lr = node.linear_reg(parseKey['Key'], colA, colB)
 
 ###     # we'll have to add something for LR.json to verify the LR results
 
@@ -54,7 +54,7 @@ def runRFOnly(node=None, parseKey=None, trees=5, depth=30,
     if not node: node = h2o.nodes[0]
     #! FIX! what else is in parseKey that we should check?
     h2o.verboseprint("runRFOnly parseKey:",parseKey)
-    key = parseKey['key']
+    key = parseKey['Key']
     rf = node.random_forest(key, trees, depth, **kwargs)
 
     # rf result json: 
