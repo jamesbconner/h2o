a80eb7e0d090a024ea9dc1df458bc76baf1b4ad7
Kevin Normoyle <kevin@0xdata.com>
2012-11-01 14:54:00 -0700

    add -b for --browse_json which can be used in a test to selective pop a browser on something you just did a json on. right now, just used for RFView in test.py -b, and to get an initial cloud view.
    test.py not affected if you don't use -b..hey matt try it with -b and
    tell me what you think! (shows the race issue is in the browser view
    also)

--- py/test.py
+++ py/test.py
@@ -1,6 +1,7 @@
 import os, json, unittest, time, shutil, sys
 import h2o_cmd
 import h2o
+import h2o_browse as h2b
 
 class Basic(unittest.TestCase):
     @classmethod
@@ -77,6 +78,9 @@ class Basic(unittest.TestCase):
         # always match the gen above!
         # kbn was failing for 46/56 trees (race)
         # reduce to get intermittent failures to lessen, for now
+
+        if (h2o.browse_json):
+            h2b.browseTheCloud()
         for x in xrange (11,60,10):
             sys.stdout.write('.')
             sys.stdout.flush()
@@ -85,11 +89,15 @@ class Basic(unittest.TestCase):
             # FIX! TBD do we always have to kick off the run from node 0?
             # what if we do another node?
             # FIX! do we need or want a random delay here?
-            h2o_cmd.runRF( trees=trees, timeoutSecs=timeoutSecs,
-                    csvPathname=csvPathname)
+            # Only pop the browsers down in RF if verbose!
+            h2o_cmd.runRF(trees=trees, timeoutSecs=timeoutSecs, csvPathname=csvPathname, browseAlso=h2o.browse_json)
             trees += 10
             ### timeoutSecs += 2
 
+        if (h2o.browse_json):
+            # just keep everything live until ctrl-c
+            print "Waiting in sleep so you can look at browser. Ctrl-C when done"
+            time.sleep(3600)
 
 if __name__ == '__main__':
     h2o.unit_main()
