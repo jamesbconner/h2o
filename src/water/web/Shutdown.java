a0af90fbec56cc2c72c0f9a2af07eb21dbe10538
Matt Fowles <matt.fowles@gmail.com>
2012-11-09 14:37:31 -0500

    HEX-192: Put a small delay on shutdown
    Allows the server to finish serving the active request

--- src/water/web/Shutdown.java
+++ src/water/web/Shutdown.java
@@ -1,22 +1,33 @@
 package water.web;
 
 import java.util.Properties;
+import java.util.TimerTask;
+
+import water.UDPRebooted;
 
 import com.google.gson.JsonObject;
 
-import water.*;
-
 public class Shutdown extends H2OPage {
   @Override public JsonObject serverJson(Server server, Properties parms, String sessionID) throws PageError {
-    UDPRebooted.global_kill(2);
+    java.util.Timer t = new java.util.Timer("Shutdown Timer");
+    t.schedule(new TimerTask() {
+      @Override
+      public void run() {
+        UDPRebooted.global_kill(2);
+        System.exit(-1);
+      }
+    }, 100);
+
+
     JsonObject json = new JsonObject();
     json.addProperty("Status", "Shutting down");
     return json;
   }
 
-  @Override public String serveImpl(Server server, Properties args, String sessionID) {
-    UDPRebooted.global_kill(2);
-    System.exit(-1);
+  @Override public String serveImpl(Server server, Properties args, String sessionID) throws PageError {
+    serverJson(server, args, sessionID);
     return "Shutting down";
   }
+
+
 }
