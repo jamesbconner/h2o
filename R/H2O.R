e1f8048e6d53a4b3ba6396c3a3b26db7bf15a9a5
thepeta <thepeta@0xdata.com>
2012-11-09 19:46:13 -0800

    New version of R interop. This one should accept also unquoted expressions and names and the code is more polished. It also works with the latest version of H2O's backend layer. I will test it more tomorrow. Add RF and GLM and then try to make an R package out of it with help & stuff for shipment.

--- R/H2O.R
+++ R/H2O.R
@@ -1,260 +1,215 @@
 library('RCurl');
 library('rjson');
 
-# H2O <-> R Interop Layer
-# 
-# We use the S3 OOP system in R for its simplicity and ease of use. Also I have learned that these are most widely used.
+# R <-> H2O Interop Layer
 #
-# An example session will thus be:
-#
-# a = H2O.Connect()
-# exec(a,"4+5")
-#
-# I know it is not the h2o(expr) that was suggested, but this is how people are used to do things in R (I was told). 
-# Please let me know any comments on the matter:)
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-H2O.SERVER = "localhost:54321"
-H2O.VERBOSE = TRUE
-H2O.MAX_RESPONSE_ITEMS = 200000
-
-h2o.help <- function() {
-  cat("This is help for H2O <-> R interop\n")
-  cat("\n")
-  cat("The following functions are supported now:\n")
-  cat("  h2o(expr) executes the expression on H2O and returns the resulting key (clipped)\n")
-  cat("  h2o.inspect(key) returns the inspection of the key\n")
-  cat("  h2o.get(key) returns the dataframe stored in given key to R\n")
-  cat("  h2o.put(key,vector) stores the vector to H2O key\n")
-  cat("  h2o.import(key,file) imports file local on the server to H2O store. and hexes it\n")
-  cat("  h2o.rf(Key, ...) invokes random forest (experimental)\n")
-}
+# This is a new version that will eventually be packaged. Comments are yet missing, will be added soon.
 
 
-h2o <- function(expr) {
-  # Executes the given expression on H2O server and returns the result as R variable. It may error if the result is
-  # too big as it is still work in progress.
-  res = h2o.exec(expr)
-  if (is.defined(res))
-    h2o.get(res)
-  else
-    NULL
-}
+#is.defined <- function(x) {
+#  # !is.null is ugly :)
+#  return (!is.null(x))
+#}
 
-h2o.connect <- function(server = H2O.SERVER, verbose = H2O.VERBOSE) {
-  # Creates the connection to the H2O server, checks its capabilities and returns the connection object. Connects to
-  # the server and obtains its cloud information. If verbose mode is enabled (by default) also prints the information
-  # as it goes.
-  H2O.SERVER = server
-  H2O.VERBOSE = verbose
-  x <- list()
-  x$server = server
-  x$verbose = verbose
-  H2O._printIfVerbose("Connecting to server ",server)
-  x$cloud <- H2O._remoteSend("Cloud.json",Client="R Interop 0.1")
-  H2O._printIfVerbose("  size ",x$cloud$cloud_size)
-  x
-}
+# Public functions & declarations -------------------------------------------------------------------------------------
 
-h2o.inspect <- function(key) {
-  # Returns the inspected key
-  H2O._printIfVerbose("  inspecting key ",key)
-  res = H2O._remoteSend("Inspect.json",Key=key)
-  if (H2O._isError(res)) {
-    H2O._printError(res$Error,prefix="  ")
-    NULL
-  } else {
-    H2O._inspectToDataFrame(res)
-  }
+h2o.SERVER = "localhost:54321"
+h2o.VERBOSE = TRUE
+h2o.MAX_GET_ROWS = 200000
+
+
+h2o <- function(expr,maxRows = h2o.MAX_GET_ROWS, forceDataFrame = FALSE) {
+  type = tryCatch({ typeof(expr) }, error = function(e) { "expr" })
+  if (type != "character")
+    expr = deparse(substitute(expr))
+  keyName = h2o.exec(expr)
+  h2o.get(keyName, maxRows, forceDataFrame)
 }
 
 h2o.exec <- function(expr) {
-  H2O._printIfVerbose("  sending expression ",expr)
-  res = H2O._remoteSend("Exec.json",Expr=expr)
-  if (H2O._isError(res)) {
-    H2O._printError(res$Error,prefix="  ")
-    NULL
-  } else {
-    res$Result
-  }
+  type = tryCatch({ typeof(expr) }, error = function(e) { "expr" })
+  if (type != "character")
+    expr = deparse(substitute(expr))
+  h2o.__printIfVerbose("  Executing expression ",expr)
+  res = h2o.__remoteSend(h2o.__PAGE_EXEC,Expr=expr)
+  res$ResultKey
 }
 
-h2o.put <- function(key, value) {
-  # Puts the given vector to the H2O cloud under the given key name. Returns the key name itself. Current limit on the
-  # vector is that it must be only a single column vector and have no more than 200000 items. 
-  H2O._printIfVerbose("  put of vector (size ",length(value),")")
-  res = H2O._remoteSend("PutVector.json",Key=key,Value=paste0(value,collapse=" "))
-  if (H2O._isError(res)) {
-    H2O._printError(res$Error,prefix="  ")
-    NULL
-  } else {
-    res$Key
-  }
+h2o.get <- function(keyName, maxRows = h2o.MAX_GET_ROWS, forceDataFrame = FALSE) {
+  type = tryCatch({ typeof(keyName) }, error = function(e) { "expr" })
+  if (type != "character")
+    keyName = deparse(substitute(keyName))
+  h2o.__printIfVerbose("  Getting key ",keyName)
+  res = h2o.__remoteSend(h2o.__PAGE_GET, Key = keyName, maxRows = maxRows)
+  h2o.__convertToRData(res,forceDataFrame = forceDataFrame)
 }
 
-h2o.import <- function(key, file, hex=TRUE) {
-  # imports url to the server. This is probably only worth our debugging. Probably. 
-  if (hex) {
-    uploadKey = file
-  } else {
-    uploadKey = key
-  }
-  H2O._printIfVerbose("  put file ",file," to key ",uploadKey)
-  res = H2O._remoteSend("ImportUrl.json",Key=uploadKey,Url=file)
-  if (!H2O._isError(res) && hex) {
-    H2O._printIfVerbose("  parsing key ",res$Key," to key ",key)
-    res = H2O._remoteSend("Parse.json",Key=res$Key, Key2=key)
-  }
-  if (H2O._isError(res)) {
-    H2O._printError(res$Error,prefix="  ")
-    NULL
-  } else {
-    res$Key
-  }
-} 
-
-h2o.remove <- function(key) {
-  # deletes the given UKV key. 
-  key = deparse(substitute(key))
-  H2O._printIfVerbose("  removing key ",key)
-  res = H2O._remoteSend("Remove.json",Key=key)
-  if (H2O._isError(res)) {
-    H2O._printError(res$Error,prefix="  ")
-    NULL
-  } else {
-    res$Key
-  }
+h2o.put <- function(keyName, value) {
+  type = tryCatch({ typeof(keyName) }, error = function(e) { "expr" })
+  if (type != "character")
+    keyName = deparse(substitute(keyName))
+  h2o.__printIfVerbose("  Putting a vector of ",length(value)," to key ",keyName)
+  res = h2o.__remoteSend(h2o.__PAGE_PUT,Key = keyName, Value = paste0(value,collapse=" "))
+  res$Key
 }
 
-h2o.rf <- function(key,ntree, depth=30,model=FALSE,gini=1,seed=42,wait=TRUE) {
-  if (model==FALSE)
-    model = paste(key,"_model",sep="")
-  H2O._printIfVerbose("  executing RF on ",key,", ",ntree," trees , maxDepth ",depth,", gini ",gini,", seed ",seed,", model key ",model)
-  res = H2O._remoteSend("RF.json",Key=key, ntree=ntree, depth=depth, gini=gini, seed=seed, modelKey=model)
-  if (H2O._isError(res)) {
-    H2O._printError(res$Error,prefix="  ")
-    NULL
-  } else {
-    H2O._printIfVerbose("    task for building ",res$ntree," from data ",res$dataKey)
-    H2O._printIfVerbose("    model key: ",res$modelKey)
-    res$modelKey
-  }
-}
-
-h2o.rfView <- function(dataKey, modelKey) {
-  H2O._printIfVerbose("  RF model request for data ",dataKey," and model ",modelKey)
-  res = H2O._remoteSend("RFView.json",dataKey=dataKey, modelKey=modelKey)
-  if (H2O._isError(res)) {
-    H2O._printError(res$Error,prefix="  ")
-    NULL
-  } else {
-    res 
-  }  
-}
-
-h2o.get <- function(key, max=H2O.MAX_RESPONSE_ITEMS, startRow=0) {
-  # returns the given key from H2O. DataFrames of multiple columns are supported as long as their number of elements,
-  # that is columns * rows is smaller than 200000.
-  H2O._printIfVerbose("  get of vector/dataframe (key ",key,")")
-  res = H2O._remoteSend("GetVector.json",Key=key,MaxItems=max, startRow=startRow)
-  if (H2O._isError(res)) {
-    H2O._printError(res$Error,prefix="  ")
-    NULL
-  } else {
-    H2O._printIfVerbose("    returned data frame of ",res$num_cols," column(s) and ",res$num_rows," row(s), first ",res$sent_rows," row(s) returned")
-    if (length(res$columns) == 1) {
-      H2O._printIfVerbose("    converting to single column vector")
-      res$columns[[1]]$contents
-      #lapply(strsplit(res$columns[[1]]$contents,split=" "),as.numeric)[[1]]
-    } else {
-      r = data.frame()
-      rows = res$sent_rows
-      for (i in 1:length(res$columns)) {
-        col = res$columns[[i]]
-        name = as.character(col$name)
-        #x = lapply(strsplit(col$contents, split=" "),as.numeric)[[1]]
-        r[1:rows, name] <- col$contents #x
-      }
-      r
-    }
-  }
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-h2o.debug.freeMem <- function(key) {
-  H2O._printIfVerbose("  freeMem of key ",key)
-  res = H2O._remoteSend("Debug.json",action="freeMem",key=key)
-  if (H2O._isError(res)) {
-    H2O._printError(res$Error,prefix="  ")
-    NULL
-  } else {
-    res
-  }
-}
-
-h2o.debug.fastParse <- function(key) {
-  H2O._printIfVerbose("  fastParse of key ",key)
-  res = H2O._remoteSend("Debug.json",action="fastParse",key=key)
-  if (H2O._isError(res)) {
-    H2O._printError(res$Error,prefix="  ")
-    NULL
-  } else {
-    res
-  }
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
-
-is.defined <- function(x) {
-  # !is.null is ugly :)
-  return (!is.null(x))
-}
-
-H2O._printIfVerbose <- function(...,prefix="") {
-  # If given connection is set to verbose, prints the given message
-  if (H2O.VERBOSE)
-    cat(paste(...,"\n",sep=""))
-}
-
-H2O._remoteSend <- function(page,...) {
-  # Sends given page request to the specified server and return its result
-  url <- paste(H2O.SERVER, page, sep = "/")
-  fromJSON(postForm(url,...))
-}
-
-H2O._printError <- function(error,prefix="") {
-  # prints given error (splits lines if necessary)
-  items = strsplit(error,"\n")[[1]];
-  for (i in 1:length(items))
-    cat(paste(prefix,items[i],"\n"))
-}
-
-H2O._isError <- function(response) {
-  # Returns true if the JSON response was is an error report
-  (is.defined(response$Error))
-}
-
-H2O._inspectToDataFrame <- function(response) {
-  # given correct inspect JSON response, converts it to a dataframe
+h2o.inspect <- function(keyName) {
+  type = tryCatch({ typeof(keyName) }, error = function(e) { "expr" })
+  if (type != "character")
+    keyName = deparse(substitute(keyName))
+  h2o.__printIfVerbose("  Inspecting key ",keyName)
+  res = h2o.__remoteSend(h2o.__PAGE_INSPECT, Key = keyName)
+  result = list()
+  result$key = res$key
+  result$type = res$type
+  result$rows = res$rows
+  result$cols = res$cols
+  result$rowSize = res$rowsize
+  result$size = res$size
+  h2o.__printIfVerbose("  key has ",res$cols," columns and ",res$rows," rows")  
+  # given correct inspect JSON response, converts its columns to a dataframe
   extract <- function(from,what) {
     result = NULL
     for (i in 1:length(from)) 
       result = c(result,from[[i]][what]);
     result;    
   }
-  res = response$columns;
-  data.frame(name = as.character(extract(res,"name")),
-             offset = as.numeric(extract(res,"off")),
-             type = as.character(extract(res,"type")),
-             size = as.numeric(extract(res,"size")),
-             base = as.numeric(extract(res,"base")),
-             scale = as.numeric(extract(res,"scale")),
-             min = as.numeric(extract(res,"min")),
-             max = as.numeric(extract(res,"max")),
-             badat = as.numeric(extract(res,"badat")),
-             mean = as.numeric(extract(res,"mean")),
-             var = as.numeric(extract(res,"var")));
+  res = res$columns;
+  result$columns = data.frame(name = as.character(extract(res,"name")),
+                              offset = as.numeric(extract(res,"off")),
+                              type = as.character(extract(res,"type")),
+                              size = as.numeric(extract(res,"size")),
+                              base = as.numeric(extract(res,"base")),
+                              scale = as.numeric(extract(res,"scale")),
+                              min = as.numeric(extract(res,"min")),
+                              max = as.numeric(extract(res,"max")),
+                              badat = as.numeric(extract(res,"badat")),
+                              mean = as.numeric(extract(res,"mean")),
+                              var = as.numeric(extract(res,"var")))
+  result
 }
+
+h2o.remove <- function(keyName) {
+  type = tryCatch({ typeof(keyName) }, error = function(e) { "expr" })
+  if (type != "character")
+    keyName = deparse(substitute(keyName))
+  h2o.__printIfVerbose("  Removing key ",keyName)
+  res = h2o.__remoteSend(h2o.__PAGE_REMOVE, Key = keyName)
+  res$Key
+}
+
+h2o.importUrl <- function(keyName, url, parse = TRUE) {
+  type = tryCatch({ typeof(keyName) }, error = function(e) { "expr" })
+  if (type != "character")
+    keyName = deparse(substitute(keyName))
+  h2o.__printIfVerbose("  Importing url ",url," to key ",keyName)
+  if (parse)
+    uploadKey = url
+  else
+    uploadKey = keyName
+  res = h2o.__remoteSend(h2o.__PAGE_IMPORT, Key = uploadKey, Url = url)
+  if (parse) {
+    h2o.__printIfVerbose("  parsing key ",uploadKey," to key ",keyName)
+    res = h2o.__remoteSend(h2o.__PAGE_PARSE, Key = uploadKey, Key2 = keyName)    
+  } 
+  res$Key
+}
+
+h2o.importFile <- function(keyName, fileName, parse = TRUE) {
+  type = tryCatch({ typeof(keyName) }, error = function(e) { "expr" })
+  if (type != "character")
+    keyName = deparse(substitute(keyName))
+  h2o.importUrl(keyName,paste("file://",fileName,sep=""),parse = parse)
+}
+
+# expression shorthands -----------------------------------------------------------------------------------------------
+
+h2o.slice <- function(keyName, startRow, length=-1, forceDataFrame = FALSE) {
+  type = tryCatch({ typeof(keyName) }, error = function(e) { "expr" })
+  if (type != "character")
+    keyName = deparse(substitute(keyName))
+  if (length == -1)
+    expr = paste("slice(",keyName,",",startRow,")",sep="")
+  else
+    expr = paste("slice(",keyName,",",startRow,",",length,")",sep="")
+  resultKey = h2o.__exec(expr)
+  h2o.__get(resultKey,maxRows = h2o.MAX_GET_ROWS, forceDataFrame)
+}
+
+# Internal functions & declarations -----------------------------------------------------------------------------------
+
+h2o.__PAGE_EXEC = "Exec.json"
+h2o.__PAGE_GET = "GetVector.json"
+h2o.__PAGE_PUT = "PutVector.json"
+h2o.__PAGE_INSPECT = "Inspect.json"
+h2o.__PAGE_REMOVE = "Remove.json"
+h2o.__PAGE_IMPORT = "ImportUrl.json"
+h2o.__PAGE_PARSE = "Parse.json"
+
+
+h2o.__printIfVerbose <- function(...) {
+  if (h2o.VERBOSE == TRUE)
+    cat(paste(...,"\n",sep=""))
+}
+
+h2o.__remoteSend <- function(page,...) {
+  # Sends the given arguments as URL arguments to the given page on the specified server
+  url = paste(h2o.SERVER,page,sep="/")
+  res = fromJSON(postForm(url,...))
+  if (is.defined(res$Error))
+    stop(paste(url," returned the following error:\n",h2o.__formatError(res$Error)))
+  res    
+}
+
+h2o.__formatError <- function(error,prefix="  ") {
+  result = ""
+  items = strsplit(error,"\n")[[1]];
+  for (i in 1:length(items))
+    result = paste(result,prefix,items[i],"\n",sep="")
+  result
+}
+
+h2o.__convertToRData <- function(res,forceDataFrame=FALSE) {
+  # converts the given response to an R vector or dataframe. Vector is used when there is only one column, otherwise dataframe is used. 
+  if (!forceDataFrame && (length(res$columns) == 1)) {
+    h2o.__printIfVerbose("  converting returned ",res$num_cols," columns and ",res$sent_rows," rows to an R vector")
+    res$columns[[1]]$contents
+  } else {
+    h2o.__printIfVerbose("  converting returned ",res$num_cols," columns and ",res$sent_rows," rows to an R data frame")
+    r = data.frame()
+    rows = res$sent_rows
+    for (i in 1:length(res$columns)) {
+      col = res$columns[[i]]
+      name = as.character(col$name)
+      r[1:rows, name] <- col$contents 
+    }
+    r
+  }
+}
+
+# h2o.rf <- function(key,ntree, depth=30,model=FALSE,gini=1,seed=42,wait=TRUE) {
+#   if (model==FALSE)
+#     model = paste(key,"_model",sep="")
+#   H2O._printIfVerbose("  executing RF on ",key,", ",ntree," trees , maxDepth ",depth,", gini ",gini,", seed ",seed,", model key ",model)
+#   res = H2O._remoteSend("RF.json",Key=key, ntree=ntree, depth=depth, gini=gini, seed=seed, modelKey=model)
+#   if (H2O._isError(res)) {
+#     H2O._printError(res$Error,prefix="  ")
+#     NULL
+#   } else {
+#     H2O._printIfVerbose("    task for building ",res$ntree," from data ",res$dataKey)
+#     H2O._printIfVerbose("    model key: ",res$modelKey)
+#     res$modelKey
+#   }
+# }
+# 
+# h2o.rfView <- function(dataKey, modelKey) {
+#   H2O._printIfVerbose("  RF model request for data ",dataKey," and model ",modelKey)
+#   res = H2O._remoteSend("RFView.json",dataKey=dataKey, modelKey=modelKey)
+#   if (H2O._isError(res)) {
+#     H2O._printError(res$Error,prefix="  ")
+#     NULL
+#   } else {
+#     res 
+#   }  
+# }
 
