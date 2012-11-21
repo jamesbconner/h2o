f2946d1cc2d478287ad561c65b4cb41708248784
srisatish <sri@0xdata.com>
2012-11-20 19:11:02 -0800

    demo sequence codified

---
+++ R/H2OTestDemo.R
@@ -0,0 +1,17 @@
+# Test R functionality for Demo
+source("./H2O.R")
+
+# Run expressions on covtype
+h2o.importFile("covtype", paste(getwd(),"../../datasets/UCI/UCI-large/covtype/covtype.data",sep="/"))
+h2o.inspect("covtype")
+h2o(slice(covtype,100,100))
+h2o(sum(covtype[12]))
+h2o.glm(covtype, Y = 12, family=binomial)
+h2o.filter(covtype, covtype[6] < mean(covtype[6]))
+h2o(covtype[1] + covtype[2] * 4 + max(covtype[6]) * covtype[1] + 7 - covtype[3])
+h2o(log(covtype[1]))
+
+# Run GLM
+h2o.importFile("prostate", paste(getwd(),"../smalldata/logreg/prostate.csv",sep="/"))
+h2o.inspect("prostate")
+
