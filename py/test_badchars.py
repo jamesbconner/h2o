85b3d84c12ff371e546d5719576fc8cb071a3a05
Kevin Normoyle <kevin@0xdata.com>
2012-11-26 17:53:50 -0800

    fails. extracted from hdfs covtype replication that was failing. broke it down to one line pass/fail cause. looking at vim, the bad line is after a line of unprintables. don't know how they got there. snipped the leading lines cause unnecessary. badchars.csv and test_badchars.py. We probably want to support these bad chars? maybe they were created from dd replication (null filling?) I may not have used cat

---
+++ py/test_badchars.py
@@ -0,0 +1,18 @@
+import unittest
+import h2o, h2o_cmd
+
+class Basic(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        h2o.build_cloud(node_count=1)
+
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+
+    def test_RF_poker_1m_rf(self):
+        csvPathname = '../smalldata/badchars.csv'
+        h2o_cmd.runRF(trees=50, timeoutSecs=10, csvPathname=csvPathname)
+
+if __name__ == '__main__':
+    h2o.unit_main()
