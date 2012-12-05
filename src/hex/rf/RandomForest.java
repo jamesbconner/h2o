06e2d4e4e4ecf0e8659be36afa3f2d92f0723972
jan vitek <janvite@yahoo.com>
2012-12-05 15:17:51 -0800

    oops

--- src/hex/rf/RandomForest.java
+++ src/hex/rf/RandomForest.java
@@ -25,7 +25,7 @@ public class RandomForest {
     Random rnd = new Random(data.seed());
     for (int i = 0; i < ntrees; ++i) {
       trees[i] = new Tree(_data,maxTreeDepth,minErrorRate,stat,features(),rnd.nextLong(), drf._treeskey, drf._modelKey,i,drf._ntrees, drf._sample, drf._numrows, ignoreColumns);
-      if (!parallelTrees || i==ntrees) DRemoteTask.invokeAll(new Tree[]{trees[i]});
+      if (!parallelTrees || i==ntrees-1) DRemoteTask.invokeAll(new Tree[]{trees[i]});
     }
     Utils.pln("All trees ("+ntrees+") done in "+ t_alltrees);
   }
