784ef0b8810abcc8a0707d87caf7ee8b97b8d85d
Kevin Normoyle <kevin@0xdata.com>
2012-12-06 01:49:31 -0800

    larger and larger covtype replications, multiple host

---
+++ py/test_GLM_from_import_hosts.py
@@ -0,0 +1,98 @@
+import os, json, unittest, time, shutil, sys
+import h2o, h2o_cmd
+import h2o_hosts
+import h2o_browse as h2b
+import h2o_import as h2i
+import time
+import random
+import copy
+
+class Basic(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        h2o_hosts.build_cloud_with_hosts(1)
+
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+
+    def test_B_importFolder_GLM_bigger_and_bigger(self):
+        # We don't drop anything from csvFilename, unlike H2O default
+        # FIX! for local 0xdata, this will be different (/home/0xdiag/datasets)
+        csvFilenameList = [
+            'covtype.data',
+            'covtype.data',
+            'covtype20x.data',
+            'covtype200x.data',
+            ]
+
+        # a browser window too, just because we can
+        h2b.browseTheCloud()
+
+        importFolderPath = '/home/0xdiag/datasets'
+        h2i.setupImportFolder(None, importFolderPath)
+        timeoutSecs = 2000
+        firstglm= {}
+        for csvFilename in csvFilenameList:
+            # creates csvFilename.hex from file in importFolder dir 
+            parseKey = h2i.parseImportFolderFile(None, csvFilename, importFolderPath)
+            print csvFilename, 'parse TimeMS:', parseKey['TimeMS']
+            print "Parse result['Key']:", parseKey['Key']
+
+            # We should be able to see the parse result?
+            inspect = h2o.nodes[0].inspect(parseKey['Key'])
+            print "\n" + csvFilename
+
+            start = time.time()
+            # can't pass lamba as kwarg because it's a python reserved word
+            # FIX! just look at X=0:1 for speed, for now
+            # xval=10, norm="L2", glm_lambda=1e-4,
+            glm = h2o_cmd.runGLMOnly(parseKey=parseKey,
+                Y=54, X='0:7', norm="L2", xval=2, family="binomial", 
+                timeoutSecs=timeoutSecs)
+
+            # different when xvalidation is used? No trainingErrorDetails?
+            h2o.verboseprint("\nglm:", glm)
+            print "GLM time", glm['time']
+
+            coefficients = glm['coefficients']
+            print "coefficients:", coefficients
+
+            tsv = glm['trainingSetValidation']
+            print "\ntrainingSetErrorRate:", tsv['trainingSetErrorRate']
+
+            print "glm end on ", csvFilename, 'took', time.time() - start, 'seconds'
+
+            h2b.browseJsonHistoryAsUrlLastMatch("GLM")
+
+            print ("expected results\n" + 
+                "coefficients: {\n" +
+                "     u'1': -0.008869905291197862,\n" +
+                "     u'0': 0.726580291279175,\n" +
+                "     u'Intercept': -0.43289099532324116\n" +
+                "}\n" +
+                "trainingSetErrorRate: 0.2681\n"
+                )
+
+            # compare this glm to last one. since the files are concatenations, 
+            # the results should be similar? 10% of first is allowed delta
+            def compareToFirstGlm(self, key, glm, firstglm):
+                delta = .1 * float(firstglm[key])
+                msg = "Too large a delta (" + str(delta) + ") comparing current and first for: " + key
+                self.assertAlmostEqual(float(glm[key]), float(firstglm[key]), delta=delta, msg=msg)
+                self.assertGreaterEqual(float(glm[key]), 0.0, key + " not >= 0.0 in current")
+
+            if not firstglm:
+                # dicts are references? Make a real copy
+                firstglm = glm.copy()
+                firsttsv = tsv.copy()
+                firstcoefficients = coefficients.copy()
+            else:
+                compareToFirstGlm(self,'0',coefficients,firstcoefficients)
+                compareToFirstGlm(self,'trainingSetErrorRate',tsv,firsttsv)
+
+            sys.stdout.write('.')
+            sys.stdout.flush() 
+
+if __name__ == '__main__':
+    h2o.unit_main()
