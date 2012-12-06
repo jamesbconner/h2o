5804d63c3680ae48588c025d66886feb07786838
thepeta <thepeta@0xdata.com>
2012-12-06 11:15:50 -0800

    [sync]

---
+++ src/water/api/PutValue.java
@@ -0,0 +1,20 @@
+
+package water.api;
+
+import com.google.gson.JsonObject;
+import java.util.Properties;
+
+/**
+ *
+ * @author peta
+ */
+public class PutValue extends Request {
+
+  
+
+  @Override
+  public void serve(JsonObject response, Properties args) {
+    throw new UnsupportedOperationException("Not supported yet.");
+  }
+
+}
