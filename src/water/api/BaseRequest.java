2d2e44d6dc609c85cbb8af84fb7e56717fd010e5
thepeta <thepeta@0xdata.com>
2012-12-10 23:36:41 -0800

    [sync]

--- src/water/api/BaseRequest.java
+++ src/water/api/BaseRequest.java
@@ -27,54 +27,6 @@ public class BaseRequest {
   // Request dispatch
   // ===========================================================================
 
-  /** Request type.
-   *
-   * Requests can have multiple types. Basic types include the plain json type
-   * in which the result is returned as a JSON object, a html type that acts as
-   * the webpage, or the html.help type that displays the extended help for the
-   * request.
-   *
-   * The html.wiki type is also added that displays the markup of the wiki that
-   * should be used to document the request as per Matt's suggestion.
-   *
-   * NOTE the requests are distinguished by their suffixes. Please make the
-   * suffix start with the dot character to avoid any problems with request
-   * names.
-   */
-  public static enum RequestType {
-    json(""), ///< json type request, a result is a JSON structure
-    www(".html"), ///< webpage request
-    help(".html.help"), ///< should display the help on the given request
-    wiki(".html.wiki") ///< displays the help for the given request in a markup for wiki
-    ;
-    /** Suffix of the request - extension of the URL.
-     */
-    public final String _suffix;
-
-    RequestType(String suffix) {
-      _suffix = suffix;
-    }
-
-    /** Returns the request type of a given URL. JSON request type is the default
-     * type when the extension from the URL cannot be determined.
-     */
-    public static RequestType requestType(String requestUrl) {
-      if (requestUrl.endsWith(www._suffix))
-        return www;
-      if (requestUrl.endsWith(help._suffix))
-        return help;
-      if (requestUrl.endsWith(wiki._suffix))
-        return wiki;
-      return json;
-    }
-
-    /** Returns the name of the request, that is the request url without the
-     * request suffix.
-     */
-    public String requestName(String requestUrl) {
-      return requestUrl.substring(0, requestUrl.length()-_suffix.length());
-    }
-  }
 
   // ---------------------------------------------------------------------------
 
