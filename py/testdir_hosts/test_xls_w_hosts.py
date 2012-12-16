06479acf1e952b618b1d9f5ba518b34a731ab5c0
Kevin Normoyle <kevin@0xdata.com>
2012-12-16 02:46:23 -0800

    this localhost config json can be used with the -cj arg, on any test for making it run on a single laptop ..it specifies 7GB heap for 2 jvms.not all tests will run successfully with that but some will. This fails solidly on my machine with "python test_xls_w_hosts.py -cj pytest_config-localhost.json"

--- py/testdir_hosts/test_xls_w_hosts.py
+++ py/testdir_hosts/test_xls_w_hosts.py
@@ -19,12 +19,13 @@ class TestExcel(unittest.TestCase):
     def test_iris_xlsx(self):
         h2o_cmd.runRF(None, h2o.find_dataset('iris/iris.xlsx'), trees=100)
 
-    # FIX! why are these timing out with smaller numbers? do we need bigger defaults in runRF?
     def test_poker_xls(self):
-        h2o_cmd.runRF(None, h2o.find_dataset('poker/poker-hand-testing.xls'), trees=51, timeoutSecs=60)
+        # was 51
+        h2o_cmd.runRF(None, h2o.find_dataset('poker/poker-hand-testing.xls'), trees=31, timeoutSecs=13)
 
     def test_poker_xlsx(self):
-        h2o_cmd.runRF(None, h2o.find_dataset('poker/poker-hand-testing.xlsx'), trees=51, timeoutSecs=60)
+        # was 51
+        h2o_cmd.runRF(None, h2o.find_dataset('poker/poker-hand-testing.xlsx'), trees=31, timeoutSecs=13)
 
 if __name__ == '__main__':
     h2o.unit_main()
