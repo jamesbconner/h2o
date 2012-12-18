ee15127c0ffc68273bb7a9e587c6f97541168f70
thepeta <thepeta@0xdata.com>
2012-12-17 19:20:04 -0800

    HTTP404 and HTTP500 show how to overwrite the build() method of the request to produce a completely different page.

--- src/water/api/HTTP404.java
+++ src/water/api/HTTP404.java
@@ -4,6 +4,7 @@ package water.api;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.util.Properties;
+import water.web.RString;
 
 /**
  *
@@ -15,6 +16,17 @@ public class HTTP404 extends Request {
 
   @Override public Response serve() {
     return Response.error(_error.value());
+  }
+
+  private static final String _html =
+            "<h3>HTTP 404 - Not Found</h3>"
+          + "<div class='alert alert-error'>%ERROR</div>"
+          ;
+
+  @Override protected String build(Response response) {
+    RString str = new RString(_html);
+    str.replace("ERROR", response.error());
+    return str.toString();
   }
 
 }
