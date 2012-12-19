6ad9aefbf16d8dda9c0aa79a585175b3cd863765
Matt Fowles <matt.fowles@gmail.com>
2012-12-18 21:02:41 -0800

    make whitespace consistent

--- src/water/api/RequestArguments.java
+++ src/water/api/RequestArguments.java
@@ -1073,7 +1073,7 @@ public class RequestArguments extends RequestStatics {
       File f = new File(input);
       if( !f.exists() )
         throw new IllegalArgumentException("File "+input+" not found!");
-      if (!f.isFile())
+      if( !f.isFile() )
         throw new IllegalArgumentException("File "+input+" is not a file!");
       return f;
     }
