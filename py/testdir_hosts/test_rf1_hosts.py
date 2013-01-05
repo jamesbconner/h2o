4dacafd37e0423d147f7f7b2f1b25a36d522e012
Kevin Normoyle <kevin@0xdata.com>
2013-01-05 13:48:06 -0800

    renamed

---
+++ py/testdir_hosts/test_rf1_hosts.py
@@ -0,0 +1,26 @@
+import unittest, sys
+sys.path.extend(['.','..','py'])
+
+# easy_install nose-testconfig if you don't have this
+from testconfig import config
+
+import h2o_cmd, h2o, h2o_hosts
+
+# Uses your username specific json: pytest_config-<username>.json
+# copy pytest_config-simple.json and modify to your needs.
+class Basic(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        h2o_hosts.build_cloud_with_hosts()
+
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+
+    def test_RF_poker_1m_rf(self):
+        csvPathname = h2o.find_file('smalldata/poker/poker1000')
+        h2o_cmd.runRF(trees=50, timeoutSecs=10, csvPathname=csvPathname)
+
+if __name__ == '__main__':
+    h2o.unit_main()
+
