e4420dd00e4b49f9420be2f99153d5abc97db060
Kevin Normoyle <kevin@0xdata.com>
2013-01-05 21:19:18 -0800

    fails with 1 jvm (stack trace)

---
+++ py/testdir_single_jvm/test_GLM_covtype_1.py
@@ -0,0 +1,31 @@
+import os, json, unittest, time, shutil, sys
+sys.path.extend(['.','..','py'])
+
+import h2o, h2o_cmd, h2o_glm
+
+class Basic(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # uses two much memory with 4?
+        h2o.build_cloud(1)
+
+    @classmethod
+    def tearDownClass(cls):
+        h2o.tear_down_cloud()
+
+    def test_B_covtype(self):
+        csvPathname = h2o.find_dataset('UCI/UCI-large/covtype/covtype.data')
+        print "\n" + csvPathname
+
+        # columns start at 0
+        Y = "54"
+        parseKey = h2o_cmd.parseFile(csvPathname=csvPathname,timeoutSecs=10)
+        start = time.time()
+        kwargs = {'Y': Y, 'norm': 'L2', 'xval': 3}
+        glm = h2o_cmd.runGLMOnly(parseKey=parseKey, timeoutSecs=120, **kwargs)
+        h2o_glm.simpleCheckGLM(self, glm, 13, **kwargs)
+
+        print "glm end on ", csvPathname, 'took', time.time() - start, 'seconds'
+
+if __name__ == '__main__':
+    h2o.unit_main()
