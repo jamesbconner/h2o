e4bb03cb47a8e90da56f886094f3ae0d2ccff5b5
Kevin Normoyle <kevin@0xdata.com>
2013-01-06 17:14:00 -0800

    apparently this test can parse/pass now

--- py/testdir_single_jvm/test_randomdatagen1.py
+++ py/testdir_single_jvm/test_randomdatagen1.py
@@ -9,7 +9,7 @@ class Basic(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         h2o.build_cloud(node_count=1)
-
+        global SYNDATASETS_DIR
         SYNDATASETS_DIR = h2o.make_syn_dir()
 
     @classmethod
@@ -17,14 +17,17 @@ class Basic(unittest.TestCase):
         h2o.tear_down_cloud()
 
     def test_A_randomdata2(self):
+        print "Using smalldata/datagen1.csv as is"
         csvPathname = h2o.find_file('smalldata/datagen1.csv')
         h2o_cmd.runRF(trees=1, timeoutSecs=10, csvPathname=csvPathname)
 
     def test_B_randomdata2_1_lineend(self):
+        print "Using smalldata/datagen1.csv to create", SYNDATASETS_DIR, "/datagen1.csv with \r instead" 
         # change lineend, case 1
-        csvPathname = h2o.find_file('smalldata/datagen1.csv')
-        infile = open(csvPathname, 'r') 
-        outfile = open(csvPathname,'w') # existing file gets erased
+        csvPathname1 = h2o.find_file('smalldata/datagen1.csv')
+        csvPathname2 = SYNDATASETS_DIR + '/datagen1_crlf.csv'
+        infile = open(csvPathname1, 'r') 
+        outfile = open(csvPathname2,'w') # existing file gets erased
 
         # assume all the test files are unix lineend. 
         # I guess there shouldn't be any "in-between" ones
@@ -34,7 +37,7 @@ class Basic(unittest.TestCase):
         infile.close()
         outfile.close()
 
-        h2o_cmd.runRF(trees=1, timeoutSecs=10, csvPathname=csvPathname)
+        h2o_cmd.runRF(trees=1, timeoutSecs=10, csvPathname=csvPathname2)
 
 
 if __name__ == '__main__':
