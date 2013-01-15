2a096a2cb48a2ffa2d659a30cdbf712b77ae58c8
Matt Fowles <matt.fowles@gmail.com>
2013-01-14 16:36:54 -0800

    HEX-360: Fix an issue with upload chunking

--- py/matt.py
+++ py/matt.py
@@ -5,14 +5,39 @@ import time
 h2o.clean_sandbox()
 h2o.parse_our_args()
 
+def write_syn_dataset(csvPathname, rowCount, headerData, rowData):
+    dsf = open(csvPathname, "w+")
+    
+    dsf.write(headerData + "\n")
+    for i in range(rowCount):
+        dsf.write(rowData + "\n")
+    dsf.close()
+
+# append!
+def append_syn_dataset(csvPathname, rowData):
+    with open(csvPathname, "a") as dsf:
+        dsf.write(rowData + "\n")
+
 try:
     print 'Building cloud'
-    #h2o.build_cloud(2, java_heap_GB=1, capture_output=False, aws_credentials='/tmp/key.props')
-    h2o.nodes = [ h2o.ExternalH2O() ]
-    print 'Importing'
-    h2o.nodes[0].import_s3('test-s3-integration')
+    #h2o.build_cloud(1,java_heap_GB=1,capture_output=False)
+    h2o.nodes = [h2o.ExternalH2O()]
+
+    print 'Generating'
+    SYNDATASETS_DIR = h2o.make_syn_dir()
+    csvFilename = "syn_prostate.csv"
+    csvPathname = SYNDATASETS_DIR + '/' + csvFilename
+    headerData = "ID,CAPSULE,AGE,RACE,DPROS,DCAPS,PSA,VOL,GLEASON"
+    rowData = "1,0,65,1,2,1,1.4,0,6"
+    write_syn_dataset(csvPathname,      99860, headerData, rowData)
+
+    append_syn_dataset(csvPathname, rowData)
+    append_syn_dataset(csvPathname, rowData)
+    start = time.time()
+    key = csvFilename + "_" + str(0)
+    key2 = csvFilename + "_" + str(0) + ".hex"
     print 'Parsing'
-    h2o.nodes[0].parse('s3:test-s3-integration/covtype.data')
+    key = h2o_cmd.parseFile(csvPathname=csvPathname, key=key, key2=key2)
     print 'Done'
     while True:
         time.sleep(0.2)
