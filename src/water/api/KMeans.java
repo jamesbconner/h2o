jamesbconner/h2o

commit 3ac1feeef7bd6c5b23b1210c85c91568ccd32302

Added init phase (KMeans||) and UI to KMeans

src/water/api/KMeans.java
@@ -1,0 +1,58 @@
+package water.api;
+
+import jsr166y.RecursiveAction;
+import water.*;
+
+import com.google.gson.JsonObject;
+
+public class KMeans extends Request {
+
+  protected final H2OHexKey       _source  = new H2OHexKey(SOURCE_KEY);
+  protected final Int             _k       = new Int(K);
+  protected final Real            _epsilon = new Real(EPSILON, 1e-6);
+  protected final HexColumnSelect _columns = new HexColumnSelect(COLS, _source, null);
+  protected final H2OKey          _dest    = new H2OKey(DEST_KEY, (Key) null);
+
+  @Override
+  protected Response serve() {
+    final ValueArray va = _source.value();
+    final Key source = va._key;
+    final int k = _k.value();
+    final double epsilon = _epsilon.value();
+    final int[] cols = _columns.value();
+    Key dest = _dest.value();
+
+    if( dest == null ) {
+      String n = source.toString();
+      int dot = n.lastIndexOf('.');
+
+      if( dot > 0 )
+        n = n.substring(0, dot);
+
+      dest = Key.make(n + ".kmeans");
+    }
+
+    try {
+      final Key dest_ = dest;
+      UKV.put(dest, new hex.KMeans.Res());
+
+      H2O.FJP_NORM.submit(new RecursiveAction() {
+        @Override
+        protected void compute() {
+          hex.KMeans.run(dest_, va, k, epsilon, cols);
+        }
+      });
+
+      JsonObject response = new JsonObject();
+      response.addProperty(RequestStatics.DEST_KEY, dest.toString());
+
+      Response r = KMeansProgress.redirect(response, dest);
+      r.setBuilder(RequestStatics.DEST_KEY, new KeyElementBuilder());
+      return r;
+    } catch( IllegalArgumentException e ) {
+      return Response.error(e.getMessage());
+    } catch( Error e ) {
+      return Response.error(e.getMessage());
+    }
+  }
+}

src/water/api/RequestArguments.java
@@ -1609,27 +1609,31 @@ public class RequestArguments extends RequestStatics {
   }
 
   // ---------------------------------------------------------------------------
-  // IgnoreHexCols
+  // HexColumnSelect
   // ---------------------------------------------------------------------------
 
   public class HexColumnSelect extends MultipleSelect<int[]> {
     public final H2OHexKey _key;
-    public final H2OHexKeyCol _classCol;
+    public final H2OHexKeyCol _avoid;
 
-    public HexColumnSelect(String name, H2OHexKey key, H2OHexKeyCol classCol) {
+    public HexColumnSelect(String name, H2OHexKey key, H2OHexKeyCol avoid) {
       super(name);
       addPrerequisite(_key = key);
-      addPrerequisite(_classCol = classCol);
+      _avoid = avoid;
     }
 
     @Override protected String[] selectValues() {
       ValueArray va = _key.value();
-      int classCol = _classCol.value();
-      String[] result = new String[va._cols.length-1]; // no class col
+      int avoid = -1, count = va._cols.length;
+      if(_avoid != null) {
+        avoid=_avoid.value();
+        count--;
+      }
+      String[] result = new String[count];
       int j = 0;
       for (int i = 0; i < va._cols.length; ++i) {
-        if (i == classCol)
-          continue; // class column cannot be ignored
+        if (i == avoid)
+          continue;
         result[j] = va._cols[i]._name == null ? String.valueOf(i) : va._cols[i]._name;
         ++j;
       }
@@ -1651,17 +1655,17 @@ public class RequestArguments extends RequestStatics {
 
     @Override protected int[] parse(String input) throws IllegalArgumentException {
       ValueArray va = _key.value();
-      int classCol = _classCol.value();
+      int avoid = _avoid != null ? _avoid.value() : -1;
       ArrayList<Integer> al = new ArrayList();
       for (String col : input.split(",")) {
         col = col.trim();
         int idx = vaColumnNameToIndex(va, col);
         if (idx == -1)
           throw new IllegalArgumentException("Column "+col+" not part of key "+va._key);
-        if (idx == classCol)
-          throw new IllegalArgumentException("Class column "+col+" cannot be ignored");
+        if (idx == avoid)
+          throw new IllegalArgumentException("Class column "+col+" cannot be selected");
         if (al.contains(idx))
-          throw new IllegalArgumentException("Column "+col+" is already ignored.");
+          throw new IllegalArgumentException("Column "+col+" is already selected.");
         al.add(idx);
       }
       int[] result = new int[al.size()];
@@ -1671,11 +1675,14 @@ public class RequestArguments extends RequestStatics {
     }
 
     @Override protected int[] defaultValue() {
-      return new int[0];
+      int[] cols = new int[_key.value()._cols.length];
+      for( int i = 0; i < cols.length; i++ )
+        cols[i]=i;
+      return cols;
     }
 
     @Override protected String queryDescription() {
-      return "Columns to be ignored by the computation";
+      return "Columns selection";
     }
   }
 
@@ -1685,7 +1692,7 @@ public class RequestArguments extends RequestStatics {
       super(name, key, classCol);
     }
     @Override protected int[] defaultValue() {
-      int classCol = _classCol.value();
+      int classCol = _avoid.value();
       ValueArray va = _key.value();
       List<Integer> res = Lists.newArrayList();
       for( int i=0; i<va._cols.length; i++ )

src/hex/KMeans.java
@@ -1,24 +1,60 @@
 package hex;
 
+import java.util.*;
+
 import water.*;
 
+/**
+ * Scalable K-Means++ (KMeans||)<br>
+ * http://theory.stanford.edu/~sergei/papers/vldb12-kmpar.pdf<br>
+ * http://www.youtube.com/watch?v=cigXAxV3XcY
+ */
 public abstract class KMeans {
+  private static final boolean DEBUG = false;
 
-  static public double[][] run(ValueArray va, int k, double epsilon, int... cols) {
-    double[][] clusters = new double[k][];
+  public static class Res extends Iced {
+    public double[][] _clusters;
+    public int        _iteration;
+  }
+
+  static public void run(Key dest, ValueArray va, int k, double epsilon, int... cols) {
+    final Res res = new Res();
+
+    // Initialize first cluster to first row
+    double[][] clusters = new double[1][];
+    clusters[0] = new double[cols.length];
     AutoBuffer bits = va.getChunk(0);
+    for( int c = 0; c < cols.length; c++ )
+      clusters[0][c] = va.datad(bits, 0, va._cols[cols[c]]);
 
-    // Initialize clusters to first rows
-    // TODO proper initialization phase
-    for( int row = 0; row < clusters.length; row++ ) {
-      clusters[row] = new double[cols.length];
+    // TODO normalize data
 
-      for( int c = 0; c < cols.length; c++ )
-        clusters[row][c] = va.datad(bits, row, va._cols[cols[c]]);
+    for( int i = 0; i < 5; i++ ) {
+      // Sum squares distances to clusters
+      Sqr sqr = new Sqr();
+      sqr._arykey = va._key;
+      sqr._cols = cols;
+      sqr._clusters = clusters;
+      sqr.invoke(va._key);
+
+      // Sample with probability inverse to square distance
+      Sampler sampler = new Sampler();
+      sampler._arykey = va._key;
+      sampler._cols = cols;
+      sampler._clusters = clusters;
+      sampler._sqr = sqr._sqr;
+      sampler._probability = k * 3; // Over-sampling
+      sampler.invoke(va._key);
+      clusters = Sampler.merge(clusters, sampler._newClusters);
+
+      res._iteration++;
+      UKV.put(dest, res);
     }
 
+    clusters = recluster(clusters, k);
+
     for( ;; ) {
-      KMeansTask task = new KMeansTask();
+      Lloyds task = new Lloyds();
       task._arykey = va._key;
       task._cols = cols;
       task._clusters = clusters;
@@ -36,23 +72,129 @@ public abstract class KMeans {
         }
       }
 
-      if( !moved ) break;
-    }
+      res._iteration++;
+      if( !moved )
+        res._clusters = task._clusters;
+      UKV.put(dest, res);
 
-    return clusters;
+      if( !moved )
+        break;
+    }
   }
 
-  public static class KMeansTask extends MRTask {
+  public static class Sqr extends MRTask {
     Key        _arykey;
     int[]      _cols;
     double[][] _clusters;
 
-    // Sums and counts for each cluster
+    // Result
+    double     _sqr;
+
+    @Override
+    public void map(Key key) {
+      if( DEBUG )
+        System.out.println("sqr map " + key + ": " + this);
+
+      assert key.home();
+      ValueArray va = ValueArray.value(DKV.get(_arykey));
+      AutoBuffer bits = va.getChunk(key);
+      int rows = bits.remaining() / va._rowsize;
+      double[] values = new double[_cols.length];
+
+      for( int row = 0; row < rows; row++ ) {
+        for( int column = 0; column < _cols.length; column++ )
+          values[column] = va.datad(bits, row, va._cols[_cols[column]]);
+
+        _sqr += minSqr(_clusters, _clusters.length, values);
+      }
+    }
+
+    @Override
+    public void reduce(DRemoteTask rt) {
+      if( DEBUG )
+        System.out.println("sqr reduce " + this);
+
+      Sqr task = (Sqr) rt;
+      _sqr += task._sqr;
+    }
+  }
+
+  public static class Sampler extends MRTask {
+    Key        _arykey;
+    int[]      _cols;
+    double[][] _clusters;
+    double     _sqr;
+    double     _probability;
+
+    // Result
+    double[][] _newClusters;
+
+    @Override
+    public void map(Key key) {
+      if( DEBUG )
+        System.out.println("sampler map " + key + ": " + this + ", sqr: " + _sqr);
+
+      assert key.home();
+      ValueArray va = ValueArray.value(DKV.get(_arykey));
+      AutoBuffer bits = va.getChunk(key);
+      int rows = bits.remaining() / va._rowsize;
+      double[] values = new double[_cols.length];
+      ArrayList<double[]> list = new ArrayList<double[]>();
+      Random rand = new Random();
+
+      for( int row = 0; row < rows; row++ ) {
+        for( int column = 0; column < _cols.length; column++ )
+          values[column] = va.datad(bits, row, va._cols[_cols[column]]);
+
+        double sqr = minSqr(_clusters, _clusters.length, values);
+
+        if( _probability * sqr > rand.nextDouble() * _sqr ) {
+          if( DEBUG )
+            System.out.println("sampled: " + Arrays.toString(values));
+
+          list.add(values.clone());
+        }
+      }
+
+      _newClusters = new double[list.size()][];
+      list.toArray(_newClusters);
+    }
+
+    @Override
+    public void reduce(DRemoteTask rt) {
+      if( DEBUG )
+        System.out.println("sampler reduce " + this);
+
+      Sampler task = (Sampler) rt;
+
+      if( _newClusters != null )
+        _newClusters = merge(_newClusters, task._newClusters);
+      else
+        _newClusters = task._newClusters;
+    }
+
+    static double[][] merge(double[][] a, double[][] b) {
+      double[][] res = new double[a.length + b.length][];
+      System.arraycopy(a, 0, res, 0, a.length);
+      System.arraycopy(b, 0, res, a.length, b.length);
+      return res;
+    }
+  }
+
+  public static class Lloyds extends MRTask {
+    Key        _arykey;
+    int[]      _cols;
+    double[][] _clusters;
+
+    // Result - sums and counts for each cluster
     double[][] _sums;
     int[]      _counts;
 
     @Override
     public void map(Key key) {
+      if( DEBUG )
+        System.out.println("KMeans map " + key + ": " + this);
+
       assert key.home();
       ValueArray va = ValueArray.value(DKV.get(_arykey));
       AutoBuffer bits = va.getChunk(key);
@@ -60,11 +202,8 @@ public abstract class KMeans {
       double[] values = new double[_cols.length];
 
       // Create result arrays
-      _sums = new double[_clusters.length][];
+      _sums = new double[_clusters.length][_cols.length];
       _counts = new int[_clusters.length];
-
-      for( int c = 0; c < _clusters.length; c++ )
-        _sums[c] = new double[_cols.length];
 
       // Find closest cluster for each row
       for( int row = 0; row < rows; row++ ) {
@@ -81,30 +220,17 @@ public abstract class KMeans {
       }
     }
 
-    public static int closest(double[][] clusters, double[] point) {
-      int min = 0;
-      double minSqr = Double.MAX_VALUE;
-
-      for( int cluster = 0; cluster < clusters.length; cluster++ ) {
-        double sqr = 0;
-
-        for( int column = 0; column < point.length; column++ ) {
-          double delta = point[column] - clusters[cluster][column];
-          sqr += delta * delta;
-        }
-
-        if( sqr < minSqr ) {
-          min = cluster;
-          minSqr = sqr;
-        }
-      }
-
-      return min;
-    }
-
     @Override
     public void reduce(DRemoteTask rt) {
-      KMeansTask task = (KMeansTask) rt;
+      if( DEBUG )
+        System.out.println("KMeans reduce " + this);
+
+      Lloyds task = (Lloyds) rt;
+
+      if( _sums == null ) {
+        _sums = new double[_clusters.length][_cols.length];
+        _counts = new int[_clusters.length];
+      }
 
       for( int cluster = 0; cluster < _clusters.length; cluster++ ) {
         for( int column = 0; column < _cols.length; column++ )
@@ -113,5 +239,68 @@ public abstract class KMeans {
         _counts[cluster] += task._counts[cluster];
       }
     }
+  }
+
+  public static double minSqr(double[][] clusters, int clusterCount, double[] point) {
+    double minSqr = Double.MAX_VALUE;
+
+    for( int cluster = 0; cluster < clusterCount; cluster++ ) {
+      double sqr = 0;
+
+      for( int column = 0; column < point.length; column++ ) {
+        double delta = point[column] - clusters[cluster][column];
+        sqr += delta * delta;
+      }
+
+      if( sqr < minSqr )
+        minSqr = sqr;
+    }
+
+    return minSqr;
+  }
+
+  public static int closest(double[][] clusters, double[] point) {
+    int min = 0;
+    double minSqr = Double.MAX_VALUE;
+
+    for( int cluster = 0; cluster < clusters.length; cluster++ ) {
+      double sqr = 0;
+
+      for( int column = 0; column < point.length; column++ ) {
+        double delta = point[column] - clusters[cluster][column];
+        sqr += delta * delta;
+      }
+
+      if( sqr < minSqr ) {
+        min = cluster;
+        minSqr = sqr;
+      }
+    }
+
+    return min;
+  }
+
+  // KMeans++ re-clustering
+  public static double[][] recluster(double[][] points, int k) {
+    double[][] res = new double[k][];
+    res[0] = points[0];
+    int count = 1;
+    Random rand = new Random();
+
+    while( count < res.length ) {
+      double sum = 0;
+      for( int i = 0; i < points.length; i++ )
+        sum += minSqr(res, count, points[i]);
+
+      double r = rand.nextDouble() * sum;
+      for( int i = 0; i < points.length; i++ ) {
+        if( r < minSqr(res, count, points[i]) ) {
+          res[count++] = points[i];
+          break;
+        }
+      }
+    }
+
+    return res;
   }
 }

.settings/org.eclipse.jdt.core.prefs
@@ -368,7 +368,7 @@ org.eclipse.jdt.core.formatter.number_of_empty_lines_to_preserve=1
 org.eclipse.jdt.core.formatter.put_empty_statement_on_new_line=true
 org.eclipse.jdt.core.formatter.tabulation.char=space
 org.eclipse.jdt.core.formatter.tabulation.size=2
-org.eclipse.jdt.core.formatter.use_on_off_tags=false
+org.eclipse.jdt.core.formatter.use_on_off_tags=true
 org.eclipse.jdt.core.formatter.use_tabs_only_for_leading_indentations=true
 org.eclipse.jdt.core.formatter.wrap_before_binary_operator=true
 org.eclipse.jdt.core.formatter.wrap_before_or_operator_multicatch=true

py/cyprien.py
@@ -1,0 +1,25 @@
+import h2o, h2o_cmd
+import h2o_browse as h2b
+import time
+import webbrowser
+
+h2o.clean_sandbox()
+h2o.parse_our_args()
+
+try:
+    print 'Building cloud'
+    #h2o.build_cloud(1, java_heap_GB=1, capture_output=False)
+    h2o.nodes = [h2o.ExternalH2O()]
+    print 'Upload'
+    f = h2o.find_file('smalldata/covtype/covtype.20k.data')
+    print 'Parse'
+    h2o_cmd.parseFile(csvPathname=f, key="covtype")
+    print 'kmeans'
+    h2o.nodes[0].kmeans('covtype.hex', 7)
+    print 'web?'
+    webbrowser.open("http://localhost:54323/KMeansProgress.html?destination_key=covtype.kmeans")
+except KeyboardInterrupt:
+    print 'Interrupted'
+finally:
+    print 'EAT THE BABIES'
+    #h2o.tear_down_cloud()

py/h2o.py
@@ -602,6 +602,13 @@ class H2O(object):
                 raise Exception(emsg)
             count += 1
         return r
+    
+    def kmeans(self, response, key, k, epsilon, **kwargs):
+        return self.__check_request(
+            requests.get(
+                url=self.__url('KMeans.json', new=True),
+                timeout=300,
+                params={"source_key": key, "k": k}))
 
     def parse(self, key, key2=None, timeoutSecs=300, retryDelaySecs=0.2, **kwargs):
         browseAlso = kwargs.pop('browseAlso',False)

py/junit.py
@@ -29,7 +29,8 @@ class JUnit(unittest.TestCase):
                     'test.ParserTest',
                     'test.MinorityClassTest',
                     'test.GLMTest',
-                    'test.RBigDataTest'
+                    'test.RBigDataTest',
+                    'test.KMeansTest'
                     ])
 
             rc = ps.wait(None)

src/water/api/Constants.java
@@ -31,6 +31,7 @@ public class Constants {
   public static final String DESCRIPTION           = "description";
   public static final String DEST_KEY              = "destination_key";
   public static final String ENUM_DOMAIN           = "enum_domain";
+  public static final String EPSILON               = "epsilon";
   public static final String ERROR                 = "error";
   public static final String FAILED                = "failed";
   public static final String FEATURES              = "features";
@@ -48,6 +49,7 @@ public class Constants {
   public static final String IGNORE                = "ignore";
   public static final String ITEMS                 = "items";
   public static final String JSON_H2O              = "h2o";
+  public static final String K                     = "k";
   public static final String KEY                   = "key";
   public static final String KEYS                  = "keys";
   public static final String LAMBDA_1              = "lambda_1";

commit 76b8d1143dd01b2377455496bd31a90fd5e1f2ad

Fix the regression to RF argument parsing
- regression from 3ac1feeef7bd6c5b23b1210c85c91568ccd32302

src/water/api/KMeans.java
@@ -10,7 +10,7 @@ public class KMeans extends Request {
   protected final H2OHexKey       _source  = new H2OHexKey(SOURCE_KEY);
   protected final Int             _k       = new Int(K);
   protected final Real            _epsilon = new Real(EPSILON, 1e-6);
-  protected final HexColumnSelect _columns = new HexColumnSelect(COLS, _source, null);
+  protected final HexIgnoreColumnSelect _columns = new HexIgnoreColumnSelect(COLS, _source);
   protected final H2OKey          _dest    = new H2OKey(DEST_KEY, (Key) null);
 
   @Override

src/water/api/RequestArguments.java
@@ -8,6 +8,7 @@ import java.util.*;
 import water.*;
 import water.web.RString;
 
+import com.google.common.base.Objects;
 import com.google.common.collect.Lists;
 import com.google.common.primitives.Ints;
 import com.google.gson.JsonObject;
@@ -1600,31 +1601,27 @@ public class RequestArguments extends RequestStatics {
   }
 
   // ---------------------------------------------------------------------------
-  // HexColumnSelect
+  // IgnoreHexCols
   // ---------------------------------------------------------------------------
 
   public class HexColumnSelect extends MultipleSelect<int[]> {
     public final H2OHexKey _key;
-    public final H2OHexKeyCol _avoid;
+    public final H2OHexKeyCol _classCol;
 
-    public HexColumnSelect(String name, H2OHexKey key, H2OHexKeyCol avoid) {
+    public HexColumnSelect(String name, H2OHexKey key, H2OHexKeyCol classCol) {
       super(name);
       addPrerequisite(_key = key);
-      _avoid = avoid;
+      addPrerequisite(_classCol = classCol);
     }
 
     @Override protected String[] selectValues() {
       ValueArray va = _key.value();
-      int avoid = -1, count = va._cols.length;
-      if(_avoid != null) {
-        avoid=_avoid.value();
-        count--;
-      }
-      String[] result = new String[count];
+      int classCol = _classCol.value();
+      String[] result = new String[va._cols.length-1]; // no class col
       int j = 0;
       for (int i = 0; i < va._cols.length; ++i) {
-        if (i == avoid)
-          continue;
+        if (i == classCol)
+          continue; // class column cannot be ignored
         result[j] = va._cols[i]._name == null ? String.valueOf(i) : va._cols[i]._name;
         ++j;
       }
@@ -1646,17 +1643,17 @@ public class RequestArguments extends RequestStatics {
 
     @Override protected int[] parse(String input) throws IllegalArgumentException {
       ValueArray va = _key.value();
-      int avoid = _avoid != null ? _avoid.value() : -1;
+      int classCol = _classCol.value();
       ArrayList<Integer> al = new ArrayList();
       for (String col : input.split(",")) {
         col = col.trim();
         int idx = vaColumnNameToIndex(va, col);
         if (idx == -1)
           throw new IllegalArgumentException("Column "+col+" not part of key "+va._key);
-        if (idx == avoid)
-          throw new IllegalArgumentException("Class column "+col+" cannot be selected");
+        if (idx == classCol)
+          throw new IllegalArgumentException("Class column "+col+" cannot be ignored");
         if (al.contains(idx))
-          throw new IllegalArgumentException("Column "+col+" is already selected.");
+          throw new IllegalArgumentException("Column "+col+" is already ignored.");
         al.add(idx);
       }
       int[] result = new int[al.size()];
@@ -1666,14 +1663,65 @@ public class RequestArguments extends RequestStatics {
     }
 
     @Override protected int[] defaultValue() {
+      return new int[0];
+    }
+
+    @Override protected String queryDescription() {
+      return "Columns to be ignored by the computation";
+    }
+  }
+
+  public class HexIgnoreColumnSelect extends MultipleSelect<int[]> {
+    public final H2OHexKey _key;
+
+    public HexIgnoreColumnSelect(String name, H2OHexKey key) {
+      super(name);
+      addPrerequisite(_key = key);
+    }
+
+    @Override protected String[] selectValues() {
+      ValueArray va = _key.value();
+      String[] result = new String[va._cols.length];
+      for (int i = 0; i < va._cols.length; ++i)
+        result[i] = Objects.firstNonNull(va._cols[i]._name, String.valueOf(i));
+      return result;
+    }
+
+    @Override protected boolean isSelected(String value) {
+      ValueArray va = _key.value();
+      int[] val = value();
+      if (val == null)
+        return false;
+      int idx = vaColumnNameToIndex(va,value);
+      for (int i = 0; i < val.length; ++i)
+        if (val[i] == idx)
+          return true;
+      return false;
+    }
+
+    @Override protected int[] parse(String input) throws IllegalArgumentException {
+      ValueArray va = _key.value();
+      ArrayList<Integer> al = new ArrayList();
+      for (String col : input.split(",")) {
+        col = col.trim();
+        int idx = vaColumnNameToIndex(va, col);
+        if (idx == -1)
+          throw new IllegalArgumentException("Column "+col+" not part of key "+va._key);
+        if (al.contains(idx))
+          throw new IllegalArgumentException("Column "+col+" is already ignored.");
+        al.add(idx);
+      }
+      return Ints.toArray(al);
+    }
+
+    @Override protected int[] defaultValue() {
       int[] cols = new int[_key.value()._cols.length];
-      for( int i = 0; i < cols.length; i++ )
-        cols[i]=i;
+      for( int i = 0; i < cols.length; i++ ) cols[i]=i;
       return cols;
     }
 
     @Override protected String queryDescription() {
-      return "Columns selection";
+      return "Columns selected";
     }
   }
 
@@ -1683,7 +1731,7 @@ public class RequestArguments extends RequestStatics {
       super(name, key, classCol);
     }
     @Override protected int[] defaultValue() {
-      int classCol = _avoid.value();
+      int classCol = _classCol.value();
       ValueArray va = _key.value();
       List<Integer> res = Lists.newArrayList();
       for( int i=0; i<va._cols.length; i++ )