a981ab8a5e73640c0f5585e801c56b629269fa4e
Kevin Normoyle <kevin@0xdata.com>
2013-01-24 12:40:27 -0800

    OOBEE wasn't updated to out_of_bag_error_estimate

--- py/testdir_single_jvm/test_rf_params_rand2_7066883810153380318.py
+++ py/testdir_single_jvm/test_rf_params_rand2_7066883810153380318.py
@@ -11,16 +11,42 @@ import h2o, h2o_cmd
 # only classes 1-7 in the 55th col
 # don't allow None on ntree..causes 50 tree default!
 print "Temporarily not using binLimit=1 to 4"
+# RFView.html?
+# data_key=poker1000.hex&
+# model_key=model&ntree=50&
+# response_variable=10&
+# class_weights=
+# &out_of_bag_error_estimate=false
+#
+# RF.html?
+# data_key=poker1000.hex&
+# response_variable=10&
+# ntree=50&
+# gini=1&
+# class_weights=0%3D1.0&
+# stratify=0&
+# model_key=model&
+# out_of_bag_error_estimate=0&
+# features=&
+# ignore=&
+# sample=67&
+# bin_limit=1024&
+# depth=2147483647&
+# seed=784834182943470027&
+# parallel=1&
+# exclusive_split_limit=
+
 paramDict = {
+    'exclusive_split_limit': [None,1,2,3,4],
     'response_variable': [None,54],
     'class_weights': [None,'1=2','2=2','3=2','4=2','5=2','6=2','7=2'],
     'ntree': [1,3,7,23],
     'model_key': ['model_keyA', '012345', '__hello'],
-    'OOBEE': ['None', 'true', 'false'],
+    'out_of_bag_error_estimate': [None,0,1],
     'gini': [None, 0, 1],
     'depth': [None, 1,10,20,100],
     'bin_limit': [None,5,10,100,1000],
-    'parallel': [None,1,1],
+    'parallel': [1],
     'ignore': [None,0,1,2,3,4,5,6,7,8,9],
     'sample': [None,20,40,60,80,100],
     'seed': [None,'0','1','11111','19823134','1231231'],
